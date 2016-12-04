91c6f92e4784e94f5403cb2810827149b61b81c3
dzikoysk <dzikoysk@dzikoysk.net>
2016-12-04 11:42:36 +0100

    Fixed TabList

--- src/main/java/net/dzikoysk/funnyguilds/data/Settings.java
+++ src/main/java/net/dzikoysk/funnyguilds/data/Settings.java
@@ -376,7 +376,7 @@ public class Settings {
             }
         }
         new PlayerListScheme(ss);
-        this.playerlistEnable = false;
+        this.playerlistEnable = pc.getBoolean("player-list-enable");
         this.playerlistInterval = pc.getInt("player-list-interval");
         this.playerlistPing = pc.getInt("player-list-ping");
         this.playerlistPatch = pc.getBoolean("player-list-patch");
