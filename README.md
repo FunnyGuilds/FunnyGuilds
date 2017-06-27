3e2f81e5aac8c4ec9636f060ed12374b7626275c
MrGregorix <contact@mrgregorix.net>
2017-06-27 12:57:03 +0200

    Release 3.9.9.13

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Project website: <a href="https://funnyguilds.net/">FunnyGuilds</a>
 <br>
 
 #### Current Version
-FunnyGuilds Cleaned 3.9.9.12 for 1.7.x
+FunnyGuilds Cleaned 3.9.9.13 for 1.7.x
 <br>
 
 #### Stats
--- src/main/java/net/dzikoysk/funnyguilds/data/configs/PluginConfig.java
+++ src/main/java/net/dzikoysk/funnyguilds/data/configs/PluginConfig.java
@@ -17,7 +17,7 @@ import java.util.*;
 @CfgComment("~-~-~-~-~-~-~-~-~-~-~-~~-~-~-~~ #")
 @CfgComment("                                #")
 @CfgComment("          FunnyGuilds           #")
-@CfgComment("        3.9.9.11 Cleaned        #")
+@CfgComment("        3.9.9.13 Cleaned        #")
 @CfgComment("                                #")
 @CfgComment("~-~-~-~-~-~-~-~-~-~-~-~~-~-~-~~ #")
 public class PluginConfig {
