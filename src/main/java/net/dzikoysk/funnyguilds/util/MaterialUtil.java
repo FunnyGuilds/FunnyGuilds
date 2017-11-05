1e6b1339e047e8a2b740786ae578df9733921e8d
Ixidi <xlinux3@gmail.com>
2017-11-05 15:55:39 +0100

    Added variable {WEAPON} to death event, and translated(custom) name of weapon's material.

---
+++ src/main/java/net/dzikoysk/funnyguilds/util/MaterialUtil.java
@@ -0,0 +1,19 @@
+package net.dzikoysk.funnyguilds.util;
+
+
+import net.dzikoysk.funnyguilds.data.Settings;
+import net.dzikoysk.funnyguilds.data.configs.PluginConfig;
+import org.bukkit.Material;
+
+public class MaterialUtil {
+    
+    public static String getMaterialName(Material material){
+        PluginConfig config = Settings.getConfig();
+        if(config.translatedMaterialsEnable && config.translatedMaterials.containsKey(material)) {
+            return StringUtils.colored(Settings.getConfig().translatedMaterials.get(material));
+        }else{
+            return StringUtils.replace(material.toString().toLowerCase(), "_", " ");
+        }
+    }
+
+}
