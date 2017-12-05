96712dbf8dc0253ee2963548a1468b3c4288f796
dzikoysk <dzikoysk@dzikoysk.net>
2017-12-05 17:27:55 +0100

    Update url

--- src/main/java/net/dzikoysk/funnyguilds/util/Version.java
+++ src/main/java/net/dzikoysk/funnyguilds/util/Version.java
@@ -6,22 +6,26 @@ import org.bukkit.entity.Player;
 
 public final class Version {
 
-    public static final String VERSION_FILE_URL = "https://raw.githubusercontent.com/FunnyGuilds/FunnyGuilds/master/updater.txt";
+    public static final String VERSION_FILE_URL = "https://funnyguilds.dzikoysk.net/latest.info";
 
     public static void isNewAvailable(final Player player) {
-        if (player.hasPermission("funnyguilds.admin") || player.isOp()) {
-            FunnyGuilds.getInstance().getServer().getScheduler().runTaskAsynchronously(FunnyGuilds.getInstance(), () -> {
-                String latest = IOUtils.getContent(VERSION_FILE_URL);
-                if (latest != null && !latest.equalsIgnoreCase(FunnyGuilds.getVersion())) {
-                    player.sendMessage("");
-                    player.sendMessage(ChatColor.DARK_GRAY + "-----------------------------------");
-                    player.sendMessage(ChatColor.GRAY + "Dostepna jest nowa wersja " + ChatColor.AQUA + "FunnyGuilds" + ChatColor.GRAY + '!');
-                    player.sendMessage(ChatColor.GRAY + "Obecna: " + ChatColor.AQUA + FunnyGuilds.getVersion());
-                    player.sendMessage(ChatColor.GRAY + "Najnowsza: " + ChatColor.AQUA + latest);
-                    player.sendMessage(ChatColor.DARK_GRAY + "-----------------------------------");
-                    player.sendMessage("");
-                }
-            });
+        if (!player.hasPermission("funnyguilds.admin") || !player.isOp()) {
+            return;
         }
+
+        FunnyGuilds.getInstance().getServer().getScheduler().runTaskAsynchronously(FunnyGuilds.getInstance(), () -> {
+            String latest = IOUtils.getContent(VERSION_FILE_URL);
+
+            if (latest != null && !latest.equalsIgnoreCase(FunnyGuilds.getVersion())) {
+                player.sendMessage("");
+                player.sendMessage(ChatColor.DARK_GRAY + "-----------------------------------");
+                player.sendMessage(ChatColor.GRAY + "Dostepna jest nowa wersja " + ChatColor.AQUA + "FunnyGuilds" + ChatColor.GRAY + '!');
+                player.sendMessage(ChatColor.GRAY + "Obecna: " + ChatColor.AQUA + FunnyGuilds.getVersion());
+                player.sendMessage(ChatColor.GRAY + "Najnowsza: " + ChatColor.AQUA + latest);
+                player.sendMessage(ChatColor.DARK_GRAY + "-----------------------------------");
+                player.sendMessage("");
+            }
+        });
     }
+
 }
