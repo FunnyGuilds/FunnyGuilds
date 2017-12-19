fd6abe5674464d5522357e69ab70c3dd4b9eff36
kacperduras <git@kacperduras.pl>
2017-12-19 19:21:03 +0100

    Add GuildMemberInviteEvent.

--- src/main/java/net/dzikoysk/funnyguilds/events/guild/member/GuildMemberInviteEvent.java
+++ src/main/java/net/dzikoysk/funnyguilds/events/guild/member/GuildMemberInviteEvent.java
@@ -3,9 +3,9 @@ package net.dzikoysk.funnyguilds.events.guild.member;
 import net.dzikoysk.funnyguilds.basic.Guild;
 import net.dzikoysk.funnyguilds.basic.User;
 
-public class GuildMemberJoinEvent extends CancellableGuildMemberEvent {
+public class GuildMemberInviteEvent extends CancellableGuildMemberEvent {
 
-    public GuildMemberJoinEvent(Guild target, User member) {
+    public GuildMemberInviteEvent(Guild target, User member) {
         super(target, member);
     }
 
