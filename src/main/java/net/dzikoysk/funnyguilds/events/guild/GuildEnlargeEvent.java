871f9568b5e82ad58466ab7a7430a57e7b1d7554
kacperduras <git@kacperduras.pl>
2017-12-19 19:08:01 +0100

    Add GuildEnlargeEvent.

---
+++ src/main/java/net/dzikoysk/funnyguilds/events/guild/GuildEnlargeEvent.java
@@ -0,0 +1,11 @@
+package net.dzikoysk.funnyguilds.events.guild;
+
+import net.dzikoysk.funnyguilds.basic.Guild;
+
+public class GuildEnlargeEvent extends CancellableGuildEvent {
+
+    public GuildEnlargeEvent(Guild guild) {
+        super(guild);
+    }
+    
+}
