06c2026a4e2cebcdaf619f07bba08a9e011b7064
kacperduras <git@kacperduras.pl>
2017-12-19 18:24:16 +0100

    Create basic classes for ranks events.

---
+++ src/main/java/net/dzikoysk/funnyguilds/events/rank/CancellableRankEvent.java
@@ -0,0 +1,24 @@
+package net.dzikoysk.funnyguilds.events.rank;
+
+import net.dzikoysk.funnyguilds.basic.Rank;
+import org.bukkit.event.Cancellable;
+
+public class CancellableRankEvent extends RankEvent implements Cancellable {
+
+    private boolean cancelled;
+
+    public CancellableRankEvent(Rank old, Rank actual) {
+        super(old, actual);
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean b) {
+        this.cancelled = b;
+    }
+
+}
