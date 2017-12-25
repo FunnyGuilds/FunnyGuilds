78e483190de75a9cd72e33ecea227959e41d000d
insertt <inserttdev@gmail.com>
2017-12-25 20:16:10 +0100

    Fixed bug with guild's info message while clicking on guild's egg (references #690)

--- src/main/java/net/dzikoysk/funnyguilds/listener/EntityInteract.java
+++ src/main/java/net/dzikoysk/funnyguilds/listener/EntityInteract.java
@@ -1,15 +1,9 @@
 package net.dzikoysk.funnyguilds.listener;
 
-import net.dzikoysk.funnyguilds.basic.Guild;
-import net.dzikoysk.funnyguilds.basic.Region;
-import net.dzikoysk.funnyguilds.basic.util.RegionUtils;
-import net.dzikoysk.funnyguilds.command.ExcInfo;
 import net.dzikoysk.funnyguilds.command.ExcPlayer;
 import net.dzikoysk.funnyguilds.data.Settings;
 import net.dzikoysk.funnyguilds.data.configs.PluginConfig;
 import net.dzikoysk.funnyguilds.util.Cooldown;
-import org.bukkit.block.BlockFace;
-import org.bukkit.entity.EnderCrystal;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -19,33 +13,23 @@ import org.bukkit.event.player.PlayerInteractEntityEvent;
 import java.util.concurrent.TimeUnit;
 
 public class EntityInteract implements Listener {
-    private final Cooldown<Player> playerInfoCooldown = new Cooldown<>();
+
+    private final Cooldown<Player> informationMessageCooldowns = new Cooldown<>();
 
     @EventHandler
     public void onInteract(PlayerInteractEntityEvent event) {
-        Entity entity = event.getRightClicked();
-        if (entity instanceof Player) {
-            final PluginConfig config = Settings.getConfig();
-            Player clicked = (Player) entity;
+        PluginConfig config = Settings.getConfig();
+        Player eventCaller = event.getPlayer();
+        Entity clickedEntity = event.getRightClicked();
 
-            if (!config.infoPlayerEnabled || (config.infoPlayerSneaking && !event.getPlayer().isSneaking()) || playerInfoCooldown.cooldown(event.getPlayer(), TimeUnit.SECONDS, config.infoPlayerCooldown)) {
+        if (clickedEntity instanceof Player) {
+            Player clickedPlayer = (Player) clickedEntity;
+
+            if (!config.infoPlayerEnabled || (config.infoPlayerSneaking && !eventCaller.isSneaking()) || informationMessageCooldowns.cooldown(eventCaller, TimeUnit.SECONDS, config.infoPlayerCooldown)) {
                 return;
             }
-            new ExcPlayer().execute(event.getPlayer(), new String[]{clicked.getName()});
-        } else if (entity instanceof EnderCrystal) {
-            EnderCrystal ec = (EnderCrystal) entity;
-            Region region = RegionUtils.getAt(ec.getLocation());
-            if (region == null) {
-                return;
-            }
-            event.setCancelled(true);
-            if (region.getCenter().getBlock().getRelative(BlockFace.UP).getLocation().toVector().equals(ec.getLocation().getBlock().getLocation().toVector())) {
-                Guild guild = region.getGuild();
-                if (guild == null) {
-                    return;
-                }
-                new ExcInfo().execute(event.getPlayer(), new String[]{guild.getTag()});
-            }
+
+            new ExcPlayer().execute(eventCaller, new String[]{clickedPlayer.getName()});
         }
     }
 
--- src/main/java/net/dzikoysk/funnyguilds/system/war/WarListener.java
+++ src/main/java/net/dzikoysk/funnyguilds/system/war/WarListener.java
@@ -2,25 +2,32 @@ package net.dzikoysk.funnyguilds.system.war;
 
 import net.dzikoysk.funnyguilds.basic.Guild;
 import net.dzikoysk.funnyguilds.command.ExcInfo;
+import net.dzikoysk.funnyguilds.data.Settings;
 import net.dzikoysk.funnyguilds.system.security.SecuritySystem;
+import net.dzikoysk.funnyguilds.util.Cooldown;
 import net.dzikoysk.funnyguilds.util.reflect.EntityUtil;
 import net.dzikoysk.funnyguilds.util.reflect.Reflections;
 import org.bukkit.entity.Player;
 
 import java.lang.reflect.Field;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 // Temp class
 public final class WarListener {
 
+    private static final Cooldown<Player> informationMessageCooldowns = new Cooldown<>();
+
     private static final Class<?> USE_ENTITY_CLASS;
     private static final Field PACKET_ID_FIELD;
     private static final Field PACKET_ACTION_FIELD;
+    private static final Field ENUM_HAND_FIELD;
 
     static {
         USE_ENTITY_CLASS = Reflections.getCraftClass("PacketPlayInUseEntity");
         PACKET_ID_FIELD = Reflections.getPrivateField(USE_ENTITY_CLASS, "a");
         PACKET_ACTION_FIELD = Reflections.getPrivateField(USE_ENTITY_CLASS, "action");
+        ENUM_HAND_FIELD = Reflections.getFixedVersion().startsWith("v1_8") ? null : Reflections.getPrivateField(USE_ENTITY_CLASS, "d");
     }
 
     public static void use(Player player, Object packet) {
@@ -39,18 +46,23 @@ public final class WarListener {
 
             int id = PACKET_ID_FIELD.getInt(packet);
             Object actionEnum = PACKET_ACTION_FIELD.get(packet);
+            Object enumHand = "";
+
+            if (ENUM_HAND_FIELD != null) {
+                enumHand = ENUM_HAND_FIELD.get(packet);
+            }
 
             if (actionEnum == null) {
                 return;
             }
 
-            call(player, id, actionEnum.toString());
+            call(player, id, actionEnum.toString(), enumHand.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static void call(Player player, int id, String action) {
+    private static void call(Player player, int id, String action, String hand) {
         for (final Map.Entry<Guild, Integer> entry : EntityUtil.getEntitesMap().entrySet()) {
             if (!entry.getValue().equals(id)) {
                 continue;
@@ -62,9 +74,18 @@ public final class WarListener {
                 return;
             }
 
-            if (action.equalsIgnoreCase("ATTACK")) {
+            if ("ATTACK".equalsIgnoreCase(action)) {
                 WarSystem.getInstance().attack(player, entry.getKey());
-            } else {
+            }
+            else if ("INTERACT_AT".equalsIgnoreCase(action)) {
+                if(informationMessageCooldowns.cooldown(player, TimeUnit.SECONDS, Settings.getConfig().infoPlayerCooldown)) {
+                    return;
+                }
+
+                if (! hand.isEmpty() && ! "MAIN_HAND".equalsIgnoreCase(hand)) {
+                    return;
+                }
+
                 ExcInfo excInfo = new ExcInfo();
                 String[] parameters = new String[]{entry.getKey().getTag()};
 
