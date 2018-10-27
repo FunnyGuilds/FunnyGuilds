de52ce230829d41d106895ad5307cde3524b6ade
Kamilkime <kamilkime@gmail.com>
2018-10-27 14:21:57 +0200

    Change hook system, fix #961

--- src/main/java/net/dzikoysk/funnyguilds/hook/PluginHook.java
+++ src/main/java/net/dzikoysk/funnyguilds/hook/PluginHook.java
@@ -5,76 +5,96 @@ import org.bukkit.Bukkit;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
 
 public final class PluginHook {
-    
+
     public static final String PLUGIN_FUNNYTAB = "FunnyTab";
     public static final String PLUGIN_WORLDGUARD = "WorldGuard";
     public static final String PLUGIN_VAULT = "Vault";
     public static final String PLUGIN_PLACEHOLDERAPI = "PlaceholderAPI";
     public static final String PLUGIN_BUNGEETABLISTPLUS = "BungeeTabListPlus";
     public static final String PLUGIN_MVDWPLACEHOLDERAPI = "MVdWPlaceholderAPI";
-    
+
     private static final List<String> HOOK_LIST = new ArrayList<>();
 
     public static void init() {
-        tryInit(PLUGIN_FUNNYTAB, FunnyTabHook::initFunnyDisabler, false);
-        
+        tryInit(PLUGIN_FUNNYTAB, () -> {
+            FunnyTabHook.initFunnyDisabler();
+            return true;
+        }, false);
+
         tryInit(PLUGIN_WORLDGUARD, () -> {
             try {
                 Class.forName("com.sk89q.worldguard.protection.flags.registry.FlagRegistry");
                 Class.forName("com.sk89q.worldguard.protection.flags.Flag");
-                
+
                 if (Bukkit.getPluginManager().getPlugin("WorldGuard").getDescription().getVersion().startsWith("7")) {
                     FunnyGuildsLogger.warning("Support for WorldGuard v7.0.0 or newer is currently unavailable");
+                    return false;
                 } else {
                     WorldGuardHook.initWorldGuard();
+                    return true;
                 }
-            } catch (ClassNotFoundException wgTooOld) {
+            } catch (final ClassNotFoundException exception) {
                 FunnyGuildsLogger.warning("FunnyGuilds supports only WorldGuard v6.2 or newer");
+                return false;
             }
         });
-        
+
         tryInit(PLUGIN_BUNGEETABLISTPLUS, () -> {
             try {
                 Class.forName("codecrafter47.bungeetablistplus.api.bukkit.Variable");
                 BungeeTabListPlusHook.initVariableHook();
+
+                return true;
+            } catch (final ClassNotFoundException exception) {
+                return false;
             }
-            catch (ClassNotFoundException ignored) {}
         });
-        
+
         tryInit(PLUGIN_MVDWPLACEHOLDERAPI, () -> {
             try {
                 Class.forName("be.maximvdw.placeholderapi.PlaceholderReplacer");
                 MVdWPlaceholderAPIHook.initPlaceholderHook();
-            } catch (ClassNotFoundException ignored) {}
+
+                return true;
+            } catch (final ClassNotFoundException exception) {
+                return false;
+            }
         });
 
-        tryInit(PLUGIN_VAULT, VaultHook::initEconomyHook);
-        tryInit(PLUGIN_PLACEHOLDERAPI, PlaceholderAPIHook::initPlaceholderHook);
+        tryInit(PLUGIN_VAULT, () -> {
+            VaultHook.initEconomyHook();
+            return true;
+        });
+
+        tryInit(PLUGIN_PLACEHOLDERAPI, () -> {
+            PlaceholderAPIHook.initPlaceholderHook();
+            return true;
+        });
     }
 
-    public static void tryInit(String plugin, Runnable init, boolean notifyIfMissing) {
-        try {
-            if (Bukkit.getPluginManager().getPlugin(plugin) != null) {
-                init.run();
-                HOOK_LIST.add(plugin);
-            } else if (notifyIfMissing) {
-                FunnyGuildsLogger.info(plugin + " plugin could not be found, some features may not be available");
+    public static void tryInit(final String plugin, final Supplier<Boolean> init, final boolean notifyIfMissing) {
+        if (Bukkit.getPluginManager().getPlugin(plugin) != null) {
+            if (!init.get()) {
+                return;
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+
+            HOOK_LIST.add(plugin);
+        } else if (notifyIfMissing) {
+            FunnyGuildsLogger.info(plugin + " plugin could not be found, some features may not be available");
         }
     }
 
-    public static void tryInit(String plugin, Runnable init) {
+    public static void tryInit(final String plugin, final Supplier<Boolean> init) {
         tryInit(plugin, init, true);
     }
 
-    public static boolean isPresent(String plugin) {
+    public static boolean isPresent(final String plugin) {
         return HOOK_LIST.contains(plugin);
     }
-    
+
     private PluginHook() {}
-    
+
 }
