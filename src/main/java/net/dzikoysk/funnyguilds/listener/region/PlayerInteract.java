2f06adf3cbcbfebb23e6dbd923b9a1d0a227d681
Kamilkime <kamilkime@gmail.com>
2018-12-16 16:45:39 +0100

    Fix player interact integration with other plugins

--- src/main/java/net/dzikoysk/funnyguilds/listener/region/PlayerInteract.java
+++ src/main/java/net/dzikoysk/funnyguilds/listener/region/PlayerInteract.java
@@ -14,6 +14,7 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -24,7 +25,7 @@ public class PlayerInteract implements Listener {
 
     private final ExcInfo infoExecutor = new ExcInfo();
     
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onInteract(PlayerInteractEvent event) {
         PluginConfig config = Settings.getConfig();
         Action eventAction = event.getAction();
