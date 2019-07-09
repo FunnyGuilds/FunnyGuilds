93b8d20ec9d40fb528d2fc7c775b1f648a71057a
insertt <inserttdev@gmail.com>
2019-07-09 22:26:54 +0200

    Spawn guild heart on player respawn

--- src/main/java/net/dzikoysk/funnyguilds/listener/region/PlayerRespawn.java
+++ src/main/java/net/dzikoysk/funnyguilds/listener/region/PlayerRespawn.java
@@ -1,7 +1,15 @@
 package net.dzikoysk.funnyguilds.listener.region;
 
+import net.dzikoysk.funnyguilds.FunnyGuilds;
+import net.dzikoysk.funnyguilds.basic.guild.Guild;
+import net.dzikoysk.funnyguilds.basic.guild.Region;
+import net.dzikoysk.funnyguilds.basic.guild.RegionUtils;
 import net.dzikoysk.funnyguilds.basic.user.User;
+import net.dzikoysk.funnyguilds.data.configs.PluginConfiguration;
+import net.dzikoysk.funnyguilds.util.nms.GuildEntityHelper;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -11,17 +19,35 @@ public class PlayerRespawn implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onRespawn(final PlayerRespawnEvent event) {
-        final User user = User.get(event.getPlayer());
-        if (!user.hasGuild()) {
+        PluginConfiguration config = FunnyGuilds.getInstance().getPluginConfiguration();
+        Player player = event.getPlayer();
+        User user = User.get(player);
+
+        if (! user.hasGuild()) {
             return;
         }
-        
-        final Location home = user.getGuild().getHome();
+
+        Location home = user.getGuild().getHome();
+
         if (home == null) {
             return;
         }
-        
-        event.setRespawnLocation(home);
-    }
 
+        event.setRespawnLocation(home);
+
+        if (config.createEntityType == null) {
+            return;
+        }
+
+        Bukkit.getScheduler().runTaskAsynchronously(FunnyGuilds.getInstance(),  () -> {
+            Region guildRegion = RegionUtils.getAt(home);
+
+            if (guildRegion == null) {
+                return;
+            }
+
+            Guild guild = guildRegion.getGuild();
+            GuildEntityHelper.spawnGuildHeart(guild, player);
+        });
+    }
 }
