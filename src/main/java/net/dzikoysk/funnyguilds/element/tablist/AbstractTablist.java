b0b17c1bad97f6ccd0d3a45ec85036846b7f3c4f
PinkLolicorn <pinklolicorngithub@daffit.pl>
2019-11-16 22:59:19 +0100

    Remove unused import

--- src/main/java/net/dzikoysk/funnyguilds/element/tablist/AbstractTablist.java
+++ src/main/java/net/dzikoysk/funnyguilds/element/tablist/AbstractTablist.java
@@ -24,7 +24,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
 
 public abstract class AbstractTablist {
 
