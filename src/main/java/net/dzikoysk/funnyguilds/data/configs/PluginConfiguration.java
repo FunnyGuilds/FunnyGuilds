7a6b881bbe8e3135efd3e2c5ac71024035513897
dzikoysk <dzikoysk@dzikoysk.net>
2021-01-12 20:41:01 +0100

    Release 4.8.1

--- src/main/java/net/dzikoysk/funnyguilds/data/configs/PluginConfiguration.java
+++ src/main/java/net/dzikoysk/funnyguilds/data/configs/PluginConfiguration.java
@@ -51,7 +51,7 @@ import java.util.concurrent.TimeUnit;
 @CfgComment("~-~-~-~-~-~-~-~-~-~-~-~~-~-~-~~ #")
 @CfgComment("                                #")
 @CfgComment("          FunnyGuilds           #")
-@CfgComment("         4.8.0 Tribute          #")
+@CfgComment("         4.8.1 Tribute          #")
 @CfgComment("                                #")
 @CfgComment("~-~-~-~-~-~-~-~-~-~-~-~~-~-~-~~ #")
 @CfgComment("FunnyGuilds wspiera PlaceholderAPI, lista dodawanych placeholderow znajduje sie tutaj:")
@@ -1056,7 +1056,7 @@ public class PluginConfiguration {
     @CfgComment("Wyglad naglowka w liscie graczy.")
     @CfgName("player-list-header")
     @CfgStringStyle(StringStyle.ALWAYS_QUOTED)
-    public String playerListHeader = "&7FunnyGuilds &b4.8.0 Tribute";
+    public String playerListHeader = "&7FunnyGuilds &b4.8.1 Tribute";
 
     @CfgComment("Wyglad stopki w liscie graczy.")
     @CfgName("player-list-footer")
