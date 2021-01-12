90609d7d4461ae2cc8345bebaa1c0227302023a2
dzikoysk <dzikoysk@dzikoysk.net>
2021-01-12 23:01:32 +0100

    GH-1397 Don't create tablist for non-prepared users (Fix #1397)

--- src/main/java/net/dzikoysk/funnyguilds/element/tablist/TablistBroadcastHandler.java
+++ src/main/java/net/dzikoysk/funnyguilds/element/tablist/TablistBroadcastHandler.java
@@ -1,6 +1,7 @@
 package net.dzikoysk.funnyguilds.element.tablist;
 
 import net.dzikoysk.funnyguilds.FunnyGuilds;
+import net.dzikoysk.funnyguilds.basic.user.User;
 import net.dzikoysk.funnyguilds.data.configs.PluginConfiguration;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -17,7 +18,13 @@ public class TablistBroadcastHandler implements Runnable {
 
         for (Player player : Bukkit.getOnlinePlayers()) {
             if (! AbstractTablist.hasTablist(player)) {
-                AbstractTablist.createTablist(config.playerList, config.playerListHeader, config.playerListFooter, config.playerListPing, player);
+                User user = User.get(player);
+
+                if (user == null) {
+                    continue;
+                }
+
+                AbstractTablist.createTablist(config.playerList, config.playerListHeader, config.playerListFooter, config.playerListPing, user);
             }
 
             AbstractTablist tablist = AbstractTablist.getTablist(player);
