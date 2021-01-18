23301878de30964a3c6a6b718c0430ccb17d22a5
dzikoysk <dzikoysk@dzikoysk.net>
2021-01-18 02:11:53 +0100

    Change priority of blocked commands to HIGH

--- src/main/java/net/dzikoysk/funnyguilds/listener/region/PlayerCommand.java
+++ src/main/java/net/dzikoysk/funnyguilds/listener/region/PlayerCommand.java
@@ -7,40 +7,48 @@ import net.dzikoysk.funnyguilds.basic.guild.RegionUtils;
 import net.dzikoysk.funnyguilds.basic.user.User;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 public class PlayerCommand implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGH)
     public void onCommand(PlayerCommandPreprocessEvent event) {
         Player player = event.getPlayer();
+
         if (player.hasPermission("funnyguilds.admin")) {
             return;
         }
 
-        String[] splited = event.getMessage().split("\\s+");
-        if (splited.length == 0) {
+        String[] commandElements = event.getMessage().split("\\s+");
+
+        if (commandElements.length == 0) {
             return;
         }
-        String command = splited[0];
-        for (String s : FunnyGuilds.getInstance().getPluginConfiguration().regionCommands) {
-            if (("/" + s).equalsIgnoreCase(command)) {
+
+        String command = commandElements[0];
+
+        for (String blockedCommand: FunnyGuilds.getInstance().getPluginConfiguration().regionCommands) {
+            if (("/" + blockedCommand).equalsIgnoreCase(command)) {
                 command = null;
                 break;
             }
         }
+
         if (command != null) {
             return;
         }
 
         Region region = RegionUtils.getAt(player.getLocation());
+
         if (region == null) {
             return;
         }
 
         Guild guild = region.getGuild();
         User user = User.get(player);
+
         if (guild.getMembers().contains(user)) {
             return;
         }
