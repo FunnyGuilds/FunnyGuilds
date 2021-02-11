535eac38571d35f47a129acfe0b0b2dd7a731fd0
Michał Kasprzyk <northpl93@gmail.com>
2021-02-11 02:20:39 +0100

    GH-1434 Add missing RUNTIME retention in validation annotations. Fixes #1434

--- src/main/java/net/dzikoysk/funnyguilds/command/CanManage.java
+++ src/main/java/net/dzikoysk/funnyguilds/command/CanManage.java
@@ -1,3 +1,7 @@
 package net.dzikoysk.funnyguilds.command;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface CanManage { }
