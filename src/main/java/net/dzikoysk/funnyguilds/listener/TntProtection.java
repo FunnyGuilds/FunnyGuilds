b6b48e6922112da2a70fd0fd69b72d9d1aa251e6
insertt <inserttdev@gmail.com>
2021-02-12 23:58:09 +0100

    GH-1410 Handle guild terrain explosions properly
    Before, when TNT (or other explosive) exploded on terrain that was not a guild terrain, but the explosion reached the blocks that were on the guild terrain, these blocks would explode even if this guild should be protected from explosions at the time.
    
    Now, we're excluding blocks from the explosion that belong to the guild's terrain that have explosion protection even if explosion source was not located on the guild's terrain.
    
    Closes #1410

--- src/main/java/net/dzikoysk/funnyguilds/listener/TntProtection.java
+++ src/main/java/net/dzikoysk/funnyguilds/listener/TntProtection.java
@@ -4,12 +4,14 @@ import net.dzikoysk.funnyguilds.FunnyGuilds;
 import net.dzikoysk.funnyguilds.basic.guild.Region;
 import net.dzikoysk.funnyguilds.basic.guild.RegionUtils;
 import net.dzikoysk.funnyguilds.data.configs.PluginConfiguration;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
 
 import java.time.LocalTime;
+import java.util.Iterator;
 
 public class TntProtection implements Listener {
 
@@ -20,8 +22,6 @@ public class TntProtection implements Listener {
         if (!config.guildTNTProtectionEnabled && !config.guildTNTProtectionGlobal) {
             return;
         }
-
-        Region region = RegionUtils.getAt(event.getLocation());
 
         LocalTime now = LocalTime.now();
         LocalTime start = config.guildTNTProtectionStartTime;
@@ -35,8 +35,27 @@ public class TntProtection implements Listener {
             return;
         }
 
-        if (config.guildTNTProtectionGlobal || (config.guildTNTProtectionEnabled && region != null)) {
+        if (config.guildTNTProtectionGlobal) {
             event.setCancelled(true);
+        }
+        else if (config.guildTNTProtectionEnabled) {
+            Region region = RegionUtils.getAt(event.getLocation());
+
+            if (region != null) {
+                event.setCancelled(true);
+                return;
+            }
+
+            Iterator<Block> affectedBlocks = event.blockList().iterator();
+
+            while (affectedBlocks.hasNext()) {
+                Block block = affectedBlocks.next();
+                Region maybeRegion = RegionUtils.getAt(block.getLocation());
+
+                if (maybeRegion != null) {
+                    affectedBlocks.remove();
+                }
+            }
         }
     }
 
