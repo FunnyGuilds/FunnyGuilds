0f47bf876c4fe30aa89c9a0f6877cec599d79dd3
arturekdev <koeckiartur@gmail.com>
2021-02-12 20:54:42 +0100

    Add suggestions.

--- src/main/java/net/dzikoysk/funnyguilds/listener/TntProtection.java
+++ src/main/java/net/dzikoysk/funnyguilds/listener/TntProtection.java
@@ -16,21 +16,26 @@ public class TntProtection implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onExplode(EntityExplodeEvent event) {
         PluginConfiguration config = FunnyGuilds.getInstance().getPluginConfiguration();
+
+        if (!config.guildTNTProtectionEnabled && !config.guildTNTProtectionGlobal) {
+            return;
+        }
+
         Region region = RegionUtils.getAt(event.getLocation());
 
         LocalTime now = LocalTime.now();
         LocalTime start = config.guildTNTProtectionStartTime;
         LocalTime end = config.guildTNTProtectionEndTime;
 
-        boolean isWithinTimeframe = config.guildTNTProtectionPassingMidnight ?
-                now.isAfter(start) || now.isBefore(end) :
-                now.isAfter(start) && now.isBefore(end);
+        boolean isWithinTimeframe = config.guildTNTProtectionPassingMidnight
+                ? now.isAfter(start) || now.isBefore(end)
+                : now.isAfter(start) && now.isBefore(end);
 
         if (!isWithinTimeframe) {
             return;
         }
 
-        if (config.guildTNTProtectionGlobal || (config.guildTNTProtectionEnabled && region != null)) {
+        if (config.guildTNTProtectionGlobal || config.guildTNTProtectionEnabled && region != null) {
             event.setCancelled(true);
         }
     }
