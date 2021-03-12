fff0af0d495b4ed1502cbc46fc07fa6511c529e2
Rollczi <ndejlich5@gmail.com>
2021-03-12 02:19:30 +0100

    Convert all of these random numbers to static consts.

--- src/main/java/net/dzikoysk/funnyguilds/system/security/cheat/SecurityReach.java
+++ src/main/java/net/dzikoysk/funnyguilds/system/security/cheat/SecurityReach.java
@@ -3,10 +3,10 @@ package net.dzikoysk.funnyguilds.system.security.cheat;
 import net.dzikoysk.funnyguilds.FunnyGuilds;
 import net.dzikoysk.funnyguilds.basic.user.User;
 import net.dzikoysk.funnyguilds.data.configs.MessageConfiguration;
+import net.dzikoysk.funnyguilds.data.configs.PluginConfiguration;
 import net.dzikoysk.funnyguilds.system.security.SecurityUtils;
 import net.dzikoysk.funnyguilds.util.commons.bukkit.MinecraftServerUtils;
 import net.dzikoysk.funnyguilds.util.commons.bukkit.PingUtils;
-import org.apache.commons.lang.StringUtils;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 
@@ -15,26 +15,30 @@ import java.text.DecimalFormat;
 public class SecurityReach {
 
     private static final DecimalFormat FORMAT = new DecimalFormat("##.##");
+    private static final double creativeReach = 4.5;
+    private static final double survivalReach = 3.0;
+    private static final double importanceOfPing = 0.93;
+    private static final double importanceOfTps = 10.0;
 
     private SecurityReach() {}
 
     public static void on(Player player, double distance) {
         MessageConfiguration messages = FunnyGuilds.getInstance().getMessageConfiguration();
-        double compensation = player.getGameMode().equals(GameMode.CREATIVE) ? 4.5 : 3.0;
-        compensation += FunnyGuilds.getInstance().getPluginConfiguration().reachCompensation;
-        compensation += SecurityUtils.compensationMs(PingUtils.getPing(player) * 0.93);
-        compensation += SecurityUtils.compensationMs((1000 / MinecraftServerUtils.getRecentTPS(0) - 50) * 10);
+        PluginConfiguration config = FunnyGuilds.getInstance().getPluginConfiguration();
+        double ping = PingUtils.getPing(player);
+        double tpsDelayMs = (1000.0 / MinecraftServerUtils.getRecentTPS(0) - 50.0);
+        double compensation = player.getGameMode().equals(GameMode.CREATIVE) ? creativeReach : survivalReach;
+
+        compensation += config.reachCompensation;
+        compensation += SecurityUtils.compensationMs(importanceOfPing * ping);
+        compensation += SecurityUtils.compensationMs(importanceOfTps * tpsDelayMs);
 
         if (distance < compensation) {
             return;
         }
 
-        String message = messages.SecuritySystemReach;
-        message = StringUtils.replace(message, "{DISTANCE}", FORMAT.format(distance));
-
+        String message = messages.SecuritySystemReach.replace("{DISTANCE}", FORMAT.format(distance));
         SecurityUtils.addVL(User.get(player));
         SecurityUtils.sendToOperator(player, "Reach", message);
     }
-
-
 }
