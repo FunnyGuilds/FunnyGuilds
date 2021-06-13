c6aeafc3204dc0225f6ffcf0f98790d36b793ebc
Norbert Dejlich <49173834+Rollczi@users.noreply.github.com>
2021-06-13 15:40:53 +0200

    GH-1578 Fix vector counting by adding correct direction (Fix #1578)

--- src/main/java/net/dzikoysk/funnyguilds/system/security/cheat/SecurityFreeCam.java
+++ src/main/java/net/dzikoysk/funnyguilds/system/security/cheat/SecurityFreeCam.java
@@ -26,8 +26,9 @@ public class SecurityFreeCam {
     public static void on(Player player, Vector origin, Vector hitPoint, double distance) {
         MessageConfiguration messages = FunnyGuilds.getInstance().getMessageConfiguration();
         PluginConfiguration config = FunnyGuilds.getInstance().getPluginConfiguration();
-        BlockIterator blockIterator = new BlockIterator(player.getWorld(), origin, hitPoint, 0, Math.max((int) distance, 1));
-        /* compensationSneaking will be removed after add the cursor height check on each client version. */
+        Vector directionToHitPoint = hitPoint.clone().subtract(origin);
+        BlockIterator blockIterator = new BlockIterator(player.getWorld(), origin, directionToHitPoint, 0, Math.max((int) distance, 1));
+        //TODO: compensationSneaking will be removed after add the cursor height check on each client version.
         int compensationSneaking = player.isSneaking() ? 1 : 0;
         List<Block> blocks = Streams.stream(blockIterator)
                 .filter(block -> !block.isLiquid())
@@ -43,7 +44,7 @@ public class SecurityFreeCam {
         }
 
         String message = messages.securitySystemFreeCam;
-        message = StringUtils.replace(message, "{BLOCKS}, ", Joiner.on(", ").join(blocks, b -> MaterialUtils.getMaterialName(b.getType())).toString());
+        message = StringUtils.replace(message, "{BLOCKS}", Joiner.on(", ").join(blocks, b -> MaterialUtils.getMaterialName(b.getType())).toString());
 
         SecurityUtils.addViolationLevel(User.get(player));
         SecurityUtils.sendToOperator(player, "FreeCam", message);
