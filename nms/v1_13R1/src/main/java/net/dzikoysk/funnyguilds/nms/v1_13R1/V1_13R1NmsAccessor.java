20bd6bf2f586822eab0df44aafbea88a7e8203de
Michał Kasprzyk <northpl93@gmail.com>
2021-07-07 21:44:28 +0200

    Create modules and NmsAccessors for all supported minecraft versions.

---
+++ nms/v1_13R1/src/main/java/net/dzikoysk/funnyguilds/nms/v1_13R1/V1_13R1NmsAccessor.java
@@ -0,0 +1,29 @@
+package net.dzikoysk.funnyguilds.nms.v1_13R1;
+
+import net.dzikoysk.funnyguilds.nms.api.NmsAccessor;
+import net.dzikoysk.funnyguilds.nms.api.entity.EntityAccessor;
+import net.dzikoysk.funnyguilds.nms.api.packet.PacketAccessor;
+import net.dzikoysk.funnyguilds.nms.api.playerlist.PlayerListAccessor;
+import net.dzikoysk.funnyguilds.nms.api.statistics.StatisticsAccessor;
+
+public class V1_13R1NmsAccessor implements NmsAccessor {
+    @Override
+    public PacketAccessor getPacketAccessor() {
+        return null;
+    }
+
+    @Override
+    public PlayerListAccessor getPlayerListAccessor() {
+        return null;
+    }
+
+    @Override
+    public StatisticsAccessor getStatisticsAccessor() {
+        return null;
+    }
+
+    @Override
+    public EntityAccessor getEntityAccessor() {
+        return null;
+    }
+}
