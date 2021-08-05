364eeef33671f74fb9a9ccb9e68ea6c637b167cf
CDFN <codefun@protonmail.com>
2021-08-05 20:08:54 +0200

    Rework replacer so it actually supports objects other than ComponentLike

--- plugin/src/main/java/net/dzikoysk/funnyguilds/util/ComponentReplacer.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/util/ComponentReplacer.java
@@ -15,13 +15,17 @@ public class ComponentReplacer {
         this.component = Objects.requireNonNull(componentLike.asComponent(), "componentLike");
     }
 
-    @Contract("_, null -> fail; null, _ -> fail")
+    @Contract("null, _ -> fail")
     public ComponentReplacer replace(String search, Object replace) {
-        Component replacement = Component.text("<?>");
+        Component replacement;
         TextReplacementConfig.Builder replacementConfigBuilder = TextReplacementConfig.builder().matchLiteral(search);
 
         if (replace instanceof ComponentLike) {
             replacement = (Component) replace;
+        } else if (replace instanceof String) {
+            replacement = Component.text((String) replace);
+        } else {
+            replacement = Component.text(replace.toString());
         }
 
         this.component = this.component.replaceText(replacementConfigBuilder.replacement(replacement).build());
