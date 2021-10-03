96dde0e9a68a2286817a4187a9a78dc6d7c5c8c5
Peridot <peridot491@protonmail.com>
2021-10-03 13:43:51 +0200

    ??????????????????????

--- plugin/src/main/java/net/dzikoysk/funnyguilds/config/sections/TntProtectionConfiguration.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/config/sections/TntProtectionConfiguration.java
@@ -2,38 +2,30 @@ package net.dzikoysk.funnyguilds.config.sections;
 
 import eu.okaeri.configs.OkaeriConfig;
 import eu.okaeri.configs.annotation.Comment;
-import eu.okaeri.configs.annotation.CustomKey;
 import eu.okaeri.configs.annotation.Exclude;
-import eu.okaeri.configs.annotation.NameStrategy;
-import eu.okaeri.configs.annotation.Names;
 
 import java.time.LocalTime;
 
-@Names(strategy = NameStrategy.IDENTITY)
 public class TntProtectionConfiguration extends OkaeriConfig {
 
     public Time time = new Time();
 
-    @Names(strategy = NameStrategy.IDENTITY)
     public static class Time extends OkaeriConfig {
 
         @Comment("Czy wlaczyc ochrone przed TNT w gildiach w podanych godzinach")
         public boolean enabled = false;
 
         @Comment("Czy wlaczyc ochrone przed TNT na całym serwerze w podanych godzinach")
-        @CustomKey("enabled-global")
         public boolean enabledGlobal = false;
 
         @Comment("O ktorej godzinie ma sie zaczac ochrona przed TNT w gildii")
         @Comment("Godzina w formacie HH:mm")
-        @CustomKey("start-time")
         public String startTime_ = "22:00";
         @Exclude
         public LocalTime startTime;
 
         @Comment("Do ktorej godziny ma dzialac ochrona przed TNT w gildii")
         @Comment("Godzina w formacie HH:mm")
-        @CustomKey("end-time")
         public String endTime_ = "06:00";
         @Exclude
         public LocalTime endTime;
@@ -44,30 +36,24 @@ public class TntProtectionConfiguration extends OkaeriConfig {
 
     public Build build = new Build();
 
-    @Names(strategy = NameStrategy.IDENTITY)
     public static class Build extends OkaeriConfig {
 
         @Comment("Minimalna wysokosc od ktorej mozna stawiac TNT.")
-        @CustomKey("min-height")
         public int minHeight = 0;
 
         @Comment("Maksymalna wysokosc do ktorej mozna stawiac TNT.")
-        @CustomKey("max-height")
         public int maxHeight = 255;
 
     }
 
     public Explode explode = new Explode();
 
-    @Names(strategy = NameStrategy.IDENTITY)
     public static class Explode extends OkaeriConfig {
 
         @Comment("Minimalna wysokosc od ktorej TNT wybucha.")
-        @CustomKey("min-height")
         public int minHeight = 0;
 
         @Comment("Maksymalna wysokosc do ktorej TNT wybucha.")
-        @CustomKey("max-height")
         public int maxHeight = 255;
 
     }
