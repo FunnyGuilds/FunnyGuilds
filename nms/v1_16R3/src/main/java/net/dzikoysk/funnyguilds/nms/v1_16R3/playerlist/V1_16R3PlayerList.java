126ba5b75410786bd9fddb0c0a40c158a5a4f8c9
Kamilkime <kamilkime@pm.me>
2021-12-11 10:36:19 +0100

    Change to empty translatable component, implement for all version

--- nms/v1_16R3/src/main/java/net/dzikoysk/funnyguilds/nms/v1_16R3/playerlist/V1_16R3PlayerList.java
+++ nms/v1_16R3/src/main/java/net/dzikoysk/funnyguilds/nms/v1_16R3/playerlist/V1_16R3PlayerList.java
@@ -20,7 +20,8 @@ import org.bukkit.craftbukkit.v1_16_R3.util.CraftChatMessage;
 import org.bukkit.entity.Player;
 
 public class V1_16R3PlayerList implements PlayerList {
-    private static final IChatBaseComponent EMPTY_COMPONENT = IChatBaseComponent.ChatSerializer.a("{\"text\":\"\"}");
+    
+    private static final IChatBaseComponent EMPTY_COMPONENT = IChatBaseComponent.ChatSerializer.a(PlayerListConstants.EMPTY_COMPONENT_VALUE);
     private static final PlayerInfoDataHelper PLAYER_INFO_DATA_HELPER = new PlayerInfoDataHelper(PacketPlayOutPlayerInfo.class, EnumGamemode.SURVIVAL);
 
     private static final Field PLAYER_INFO_DATA_ACCESSOR;
