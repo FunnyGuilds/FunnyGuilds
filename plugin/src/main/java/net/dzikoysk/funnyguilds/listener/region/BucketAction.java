96d14b0f2c8cf6b3cc3467f33d174cc4e444308b
Inder00 <inder00@wp.pl>
2022-01-04 22:11:45 +0100

    Add ability to fill and pour buckets on guild regions
    Co-authored-by: Rafał Chomczyk <ra.chomczyk@gmail.com>
    Co-authored-by: verdun <34871541+Verdun1337@users.noreply.github.com>
    Co-authored-by: arturekdev <koeckiartur@gmail.com>
    Co-authored-by: sadcenter <55924469+sadcenter@users.noreply.github.com>
    Co-authored-by: ticzuu <74562244+ticzuu@users.noreply.github.com>
    Co-authored-by: Karol Dronia <80169196+eripe14@users.noreply.github.com>
    Co-authored-by: Kamil <40739398+KamlaXPL@users.noreply.github.com>
    Co-authored-by: KinczeK <67959644+KinczeK@users.noreply.github.com>

--- plugin/src/main/java/net/dzikoysk/funnyguilds/listener/region/BucketAction.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/listener/region/BucketAction.java
@@ -2,6 +2,10 @@ package net.dzikoysk.funnyguilds.listener.region;
 
 import net.dzikoysk.funnyguilds.feature.protection.ProtectionSystem;
 import net.dzikoysk.funnyguilds.listener.AbstractFunnyListener;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerBucketEmptyEvent;
 import org.bukkit.event.player.PlayerBucketFillEvent;
@@ -10,16 +14,50 @@ public class BucketAction extends AbstractFunnyListener {
 
     @EventHandler
     public void onFill(PlayerBucketFillEvent event) {
-        ProtectionSystem.isProtected(event.getPlayer(), event.getBlockClicked().getLocation(), false)
-                .peek(result -> event.setCancelled(true))
-                .peek(ProtectionSystem::defaultResponse);
+        Player player = event.getPlayer();
+        Block block = event.getBlockClicked();
+        Material type = block.getType();
+        Location blockLocation = block.getLocation();
+
+        boolean isProtected = ProtectionSystem.isProtected(player, blockLocation, true)
+                .peek(ProtectionSystem::defaultResponse)
+                .isPresent();
+
+        if (!isProtected) {
+            return;
+        }
+
+        if (config.placingBlocksBypassOnRegion.contains(type)) {
+            return;
+        }
+
+        // always cancel to prevent breaking other protection
+        // plugins or plugins using PlayerBucketFillEvent (eg. special ability blocks)
+        event.setCancelled(true);
     }
 
     @EventHandler
     public void onEmpty(PlayerBucketEmptyEvent event) {
-        ProtectionSystem.isProtected(event.getPlayer(), event.getBlockClicked().getLocation(), false)
-                .peek(result -> event.setCancelled(true))
-                .peek(ProtectionSystem::defaultResponse);
+        Player player = event.getPlayer();
+        Block block = event.getBlockClicked();
+        Material type = event.getBucket();
+        Location blockLocation = block.getLocation();
+
+        boolean isProtected = ProtectionSystem.isProtected(player, blockLocation, true)
+                .peek(ProtectionSystem::defaultResponse)
+                .isPresent();
+
+        if (!isProtected) {
+            return;
+        }
+
+        if (config.placingBlocksBypassOnRegion.contains(type)) {
+            return;
+        }
+
+        // always cancel to prevent breaking other protection
+        // plugins or plugins using PlayerBucketFillEvent (eg. special ability blocks)
+        event.setCancelled(true);
     }
 
 }
