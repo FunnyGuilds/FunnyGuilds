73f44510d07544c42b2e74b2cfac129f5c2c6f3c
Dzikoysk <dzikoysk@dzikoysk.net>
2022-01-05 14:19:01 +0100

    Code refactor

--- plugin/src/main/java/net/dzikoysk/funnyguilds/listener/region/BucketAction.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/listener/region/BucketAction.java
@@ -2,10 +2,6 @@ package net.dzikoysk.funnyguilds.listener.region;
 
 import net.dzikoysk.funnyguilds.feature.protection.ProtectionSystem;
 import net.dzikoysk.funnyguilds.listener.AbstractFunnyListener;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerBucketEmptyEvent;
 import org.bukkit.event.player.PlayerBucketFillEvent;
@@ -15,7 +11,7 @@ public class BucketAction extends AbstractFunnyListener {
     @EventHandler
     public void onFill(PlayerBucketFillEvent event) {
         ProtectionSystem.isProtected(event.getPlayer(), event.getBlockClicked().getLocation(), true)
-                .filter(predicate -> !config.placingBlocksBypassOnRegion.contains(event.getBlockClicked().getType()))
+                .filterNot(predicate -> config.placingBlocksBypassOnRegion.contains(event.getBlockClicked().getType()))
                 .peek(result -> event.setCancelled(true))
                 .peek(ProtectionSystem::defaultResponse);
     }
@@ -23,7 +19,7 @@ public class BucketAction extends AbstractFunnyListener {
     @EventHandler
     public void onEmpty(PlayerBucketEmptyEvent event) {
         ProtectionSystem.isProtected(event.getPlayer(), event.getBlockClicked().getLocation(), true)
-                .filter(predicate -> !config.placingBlocksBypassOnRegion.contains(event.getBucket()))
+                .filterNot(predicate -> config.placingBlocksBypassOnRegion.contains(event.getBucket()))
                 .peek(result -> event.setCancelled(true))
                 .peek(ProtectionSystem::defaultResponse);
     }
