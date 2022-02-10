02d8b00ade845c3d768969094ef3530c8aa54549
Peridot <peridot491@protonmail.com>
2022-02-10 23:59:56 +0100

    Improve comments

--- plugin/src/main/java/net/dzikoysk/funnyguilds/config/sections/TopConfiguration.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/config/sections/TopConfiguration.java
@@ -17,13 +17,20 @@ import net.dzikoysk.funnyguilds.config.RawString;
 @Names(strategy = NameStrategy.HYPHEN_CASE, modifier = NameModifier.TO_LOWER_CASE)
 public class TopConfiguration extends OkaeriConfig {
 
+    @Comment("Lista topek graczy, ktore maja byc obliczane i parsowane.")
+    @Comment("Najlepiej wlaczyc tylko te, ktore beda uzywane, zeby niepotrzebnie nie obciazac serwera.")
+    @Comment("Topki te mozna uzywac na formie placeholderow {PTOP-typ-x} np. na tabie oraz w placeholderach.")
     @Comment("Dostepne typy topek: points, kills, deaths, assists, logouts")
     public Set<String> enabledUserTops = new TreeSet<>(Arrays.asList("points", "kills", "deaths"));
 
+    @Comment("Lista topek gildii, ktore maja byc obliczane i parsowane.")
+    @Comment("Najlepiej wlaczyc tylko te, ktore beda uzywane, zeby niepotrzebnie nie obciazac serwera.")
+    @Comment("Topki te mozna uzywac na formie placeholderow {GTOP-typ-x} np. na tabie oraz w placeholderach.")
     @Comment("Dostepne typy topek: points, kills, deaths, assists, logouts, avg_points, avg_kills, avg_deaths, avg_assists, avg_logouts")
     public Set<String> enabledGuildTops = new TreeSet<>(Arrays.asList("kills", "deaths", "avg_points"));
 
-    @Comment("Czy placeholdery {PTOP-x} oraz {GTOP-x} maja byc wlaczone. Wylaczenie tej funkcji moze minimalnie zwiekszyc wydajnosc.")
+    @Comment("Czy placeholdery {PTOP-x} oraz {GTOP-x} maja byc wlaczone na tabie.")
+    @Comment("Wylaczenie tej funkcji moze minimalnie odciazyc serwer, wiec jesli to mozliwe najlepiej ja wylaczyc.")
     public boolean enableLegacyPlaceholders = true;
 
     public Format format = new Format();
@@ -40,6 +47,11 @@ public class TopConfiguration extends OkaeriConfig {
         @Comment("Jesli nie chcesz wyswietlac wartosci, tylko sam tag - nie podawaj tu nic")
         public RawString gtop = new RawString(" &7[&b{VALUE}&7]");
 
+        @Comment("Wyglad znacznikika {VALUE-FORMAT} w zaleznosci od wartosci topki")
+        @Comment("Lista powinna byc podana od najmniejszych do najwiekszych rankingow")
+        @Comment("Elementy listy powinny byc postaci: \"minValue-maxValue wyglad\", np.: \"0-750 &4{VALUE}\"")
+        @Comment("Pamietaj, aby kazdy mozliwy ranking mial ustalony format!")
+        @Comment("* uzyta w zapisie elementu listy oznacza wszystkie wartosci od danego minRank w gore, np.: \"1500-* &6&l{POINTS}\"")
         public Map<String, List<NumberFormatting>> valueFormatting = ImmutableMap.<String, List<NumberFormatting>>builder()
                 .put("ptop_points", Arrays.asList(
                         new NumberFormatting(0, 749, "&4{VALUE}"),
