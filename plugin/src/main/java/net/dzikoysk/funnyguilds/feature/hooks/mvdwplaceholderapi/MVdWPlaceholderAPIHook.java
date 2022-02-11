60bb6b515cbb8024d5db502c60d5c2a2e8c4ab1a
Peridot <peridot491@protonmail.com>
2022-02-11 20:12:47 +0100

    Add MVdWPlaceholderAPI support for new top system

--- plugin/src/main/java/net/dzikoysk/funnyguilds/feature/hooks/mvdwplaceholderapi/MVdWPlaceholderAPIHook.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/feature/hooks/mvdwplaceholderapi/MVdWPlaceholderAPIHook.java
@@ -3,9 +3,11 @@ package net.dzikoysk.funnyguilds.feature.hooks.mvdwplaceholderapi;
 import be.maximvdw.placeholderapi.PlaceholderAPI;
 import java.util.Map.Entry;
 import net.dzikoysk.funnyguilds.FunnyGuilds;
+import net.dzikoysk.funnyguilds.config.PluginConfiguration;
 import net.dzikoysk.funnyguilds.feature.hooks.AbstractPluginHook;
 import net.dzikoysk.funnyguilds.feature.tablist.variable.DefaultTablistVariables;
 import net.dzikoysk.funnyguilds.feature.tablist.variable.TablistVariable;
+import net.dzikoysk.funnyguilds.rank.RankManager;
 import net.dzikoysk.funnyguilds.rank.RankUtils;
 import net.dzikoysk.funnyguilds.user.User;
 import net.dzikoysk.funnyguilds.user.UserManager;
@@ -24,7 +26,9 @@ public class MVdWPlaceholderAPIHook extends AbstractPluginHook {
 
     @Override
     public HookInitResult init() {
+        PluginConfiguration pluginConfiguration = this.plugin.getPluginConfiguration();
         UserManager userManager = this.plugin.getUserManager();
+        RankManager rankManager = this.plugin.getRankManager();
 
         for (Entry<String, TablistVariable> variable : DefaultTablistVariables.getFunnyVariables().entrySet()) {
             PlaceholderAPI.registerPlaceholder(plugin, "funnyguilds_" + variable.getKey(), event -> {
@@ -39,19 +43,43 @@ public class MVdWPlaceholderAPIHook extends AbstractPluginHook {
             });
         }
 
-        // Guild TOP, positions 1-100
-        for (int i = 1; i <= 100; i++) {
-            final int index = i;
-            PlaceholderAPI.registerPlaceholder(plugin, "funnyguilds_gtop-" + index, event -> {
-                User user = userManager.findByPlayer(event.getPlayer()).getOrNull();
-                return RankUtils.parseRank(user, "{GTOP-" + index + "}");
-            });
-        }
-
         // User TOP, positions 1-100
         for (int i = 1; i <= 100; i++) {
             final int index = i;
-            PlaceholderAPI.registerPlaceholder(plugin, "funnyguilds_ptop-" + index, event -> RankUtils.parseRank(null, "{PTOP-" + index + "}"));
+
+            rankManager.getUserTopMap().forEach((id, top) -> {
+                PlaceholderAPI.registerPlaceholder(plugin, "funnyguilds_ptop-" + id + "-" + index, event -> {
+                    User user = userManager.findByPlayer(event.getPlayer()).getOrNull();
+                    return RankUtils.parseTop(user, "{PTOP-" + id.toUpperCase() + "-" + index + "}");
+                });
+            });
+
+            if (pluginConfiguration.top.enableLegacyPlaceholders) {
+                PlaceholderAPI.registerPlaceholder(plugin, "funnyguilds_ptop-" + index, event -> {
+                    User user = userManager.findByPlayer(event.getPlayer()).getOrNull();
+                    return RankUtils.parseRank(user, "{PTOP-" + index + "}");
+                });
+            }
+        }
+
+
+        // Guild TOP, positions 1-100
+        for (int i = 1; i <= 100; i++) {
+            final int index = i;
+
+            rankManager.getGuildTopMap().forEach((id, top) -> {
+                PlaceholderAPI.registerPlaceholder(plugin, "funnyguilds_gtop-" + id + "-" + index, event -> {
+                    User user = userManager.findByPlayer(event.getPlayer()).getOrNull();
+                    return RankUtils.parseTop(user, "{GTOP-" + id.toUpperCase() + "-" + index + "}");
+                });
+            });
+
+            if (pluginConfiguration.top.enableLegacyPlaceholders) {
+                PlaceholderAPI.registerPlaceholder(plugin, "funnyguilds_gtop-" + index, event -> {
+                    User user = userManager.findByPlayer(event.getPlayer()).getOrNull();
+                    return RankUtils.parseRank(user, "{GTOP-" + index + "}");
+                });
+            }
         }
 
         return HookInitResult.SUCCESS;
