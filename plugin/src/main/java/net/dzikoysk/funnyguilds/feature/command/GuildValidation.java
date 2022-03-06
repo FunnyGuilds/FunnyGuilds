420320eb2e044371a9f7ff5933b6f59db5ec9983
Peridot <peridot491@protonmail.com>
2022-03-06 19:19:50 +0100

    GH-1849 Fix case-sensitivity in GuildValidation

--- plugin/src/main/java/net/dzikoysk/funnyguilds/feature/command/GuildValidation.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/feature/command/GuildValidation.java
@@ -2,7 +2,10 @@ package net.dzikoysk.funnyguilds.feature.command;
 
 import net.dzikoysk.funnycommands.resources.ValidationException;
 import net.dzikoysk.funnyguilds.FunnyGuilds;
+import net.dzikoysk.funnyguilds.config.MessageConfiguration;
+import net.dzikoysk.funnyguilds.config.PluginConfiguration;
 import net.dzikoysk.funnyguilds.guild.Guild;
+import net.dzikoysk.funnyguilds.guild.GuildManager;
 import org.apache.commons.lang3.StringUtils;
 
 public final class GuildValidation {
@@ -10,8 +13,20 @@ public final class GuildValidation {
     private GuildValidation() {}
 
     public static Guild requireGuildByTag(String tag) {
-        return FunnyGuilds.getInstance().getGuildManager().findByTag(tag).orThrow(() -> {
-            throw new ValidationException(StringUtils.replace(FunnyGuilds.getInstance().getMessageConfiguration().generalGuildNotExists, "{TAG}", tag));
+        FunnyGuilds plugin = FunnyGuilds.getInstance();
+
+        PluginConfiguration pluginConfiguration = plugin.getPluginConfiguration();
+        MessageConfiguration messageConfiguration = plugin.getMessageConfiguration();
+
+        GuildManager guildManager = plugin.getGuildManager();
+
+        if (!pluginConfiguration.guildTagKeepCase) {
+            tag = pluginConfiguration.guildTagUppercase ? tag.toUpperCase() : tag.toLowerCase();
+        }
+
+        String finalTag = tag;
+        return guildManager.findByTag(tag).orThrow(() -> {
+            throw new ValidationException(StringUtils.replace(messageConfiguration.generalGuildNotExists, "{TAG}", finalTag));
         });
     }
 
