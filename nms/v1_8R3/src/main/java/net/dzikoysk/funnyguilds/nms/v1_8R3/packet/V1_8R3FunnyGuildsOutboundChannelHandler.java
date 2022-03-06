e320382356582e7643e595e4d671990e3f979fd8
Peridot <peridot491@protonmail.com>
2022-03-06 23:56:03 +0100

    F**k you 1.8.8
    Signed-off-by: Peridot <peridot491@protonmail.com>

--- nms/v1_8R3/src/main/java/net/dzikoysk/funnyguilds/nms/v1_8R3/packet/V1_8R3FunnyGuildsOutboundChannelHandler.java
+++ nms/v1_8R3/src/main/java/net/dzikoysk/funnyguilds/nms/v1_8R3/packet/V1_8R3FunnyGuildsOutboundChannelHandler.java
@@ -4,10 +4,13 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelOutboundHandlerAdapter;
 import io.netty.channel.ChannelPromise;
 import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.HashSet;
 import net.dzikoysk.funnyguilds.nms.api.entity.FakeEntity;
 import net.dzikoysk.funnyguilds.nms.api.packet.FunnyGuildsOutboundChannelHandler;
 import net.dzikoysk.funnyguilds.nms.api.packet.PacketSuppliersRegistry;
 import net.minecraft.server.v1_8_R3.PacketPlayOutMapChunk;
+import net.minecraft.server.v1_8_R3.PacketPlayOutMapChunkBulk;
 
 public class V1_8R3FunnyGuildsOutboundChannelHandler extends ChannelOutboundHandlerAdapter implements FunnyGuildsOutboundChannelHandler {
 
@@ -16,21 +19,10 @@ public class V1_8R3FunnyGuildsOutboundChannelHandler extends ChannelOutboundHand
     @Override
     public void write(final ChannelHandlerContext ctx, final Object msg, final ChannelPromise promise) throws Exception {
         if (msg instanceof PacketPlayOutMapChunk) {
-            PacketPlayOutMapChunk chunkPacket = (PacketPlayOutMapChunk) msg;
-
-            int[] mapChunkCoordinates = chunkCoordinates(chunkPacket);
-
-            for (FakeEntity fakeEntity : packetSuppliersRegistry.supplyFakeEntities()) {
-                Object spawnPacket = fakeEntity.getSpawnPacket();
-
-                int[] spawnChunkCoordinates = fakeEntity.getChunkCoordinates();
-
-                if(spawnChunkCoordinates[0] != mapChunkCoordinates[0]
-                        || spawnChunkCoordinates[1] != mapChunkCoordinates[1]) {
-                    continue;
-                }
-
-                ctx.write(spawnPacket);
+            writeFakeEntities(ctx, chunkCoordinates((PacketPlayOutMapChunk) msg));
+        } else if (msg instanceof PacketPlayOutMapChunkBulk) {
+            for (int[] chunkCoordinates : chunksCoordinates((PacketPlayOutMapChunkBulk) msg)) {
+                writeFakeEntities(ctx, chunkCoordinates);
             }
         }
         super.write(ctx, msg, promise);
@@ -45,6 +37,36 @@ public class V1_8R3FunnyGuildsOutboundChannelHandler extends ChannelOutboundHand
         int zChunk = (int) chunkZField.get(chunkPacket);
 
         return new int[] {xChunk, zChunk};
+    }
+
+    private Collection<int[]> chunksCoordinates(PacketPlayOutMapChunkBulk chunkBulkPacket) throws NoSuchFieldException, IllegalAccessException {
+        Field chunksXField = chunkBulkPacket.getClass().getDeclaredField("a");
+        Field chunksZField = chunkBulkPacket.getClass().getDeclaredField("b");
+        chunksXField.setAccessible(true);
+        chunksZField.setAccessible(true);
+        int[]xChunks = (int[]) chunksXField.get(chunkBulkPacket);
+        int[]zChunks = (int[]) chunksZField.get(chunkBulkPacket);
+
+        HashSet<int[]> chunksCoordinates = new HashSet<>();
+        for (int i = 0; i < xChunks.length; i++) {
+            chunksCoordinates.add(new int[] {xChunks[i], zChunks[i]});
+        }
+        return chunksCoordinates;
+    }
+
+    private void writeFakeEntities(ChannelHandlerContext ctx, int[] chunkCoordinates) {
+        for (FakeEntity fakeEntity : packetSuppliersRegistry.supplyFakeEntities()) {
+            Object spawnPacket = fakeEntity.getSpawnPacket();
+
+            int[] spawnChunkCoordinates = fakeEntity.getChunkCoordinates();
+
+            if(spawnChunkCoordinates[0] != chunkCoordinates[0]
+                    || spawnChunkCoordinates[1] != chunkCoordinates[1]) {
+                continue;
+            }
+
+            ctx.write(spawnPacket);
+        }
     }
 
     @Override
