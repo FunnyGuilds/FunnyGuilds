2f9aa4e89522b19414e0f83834739baca6570286
Peridot <peridot491@protonmail.com>
2022-03-07 14:42:18 +0100

    Cleanup in PlayerRespawn.java

--- plugin/src/main/java/net/dzikoysk/funnyguilds/listener/region/PlayerRespawn.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/listener/region/PlayerRespawn.java
@@ -3,29 +3,17 @@ package net.dzikoysk.funnyguilds.listener.region;
 import net.dzikoysk.funnyguilds.guild.Guild;
 import net.dzikoysk.funnyguilds.listener.AbstractFunnyListener;
 import net.dzikoysk.funnyguilds.user.User;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerRespawnEvent;
-import panda.std.Option;
 
 public class PlayerRespawn extends AbstractFunnyListener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onRespawn(final PlayerRespawnEvent event) {
-        Player player = event.getPlayer();
-
-        Option<User> userOption = this.userManager.findByPlayer(player);
-        if (userOption.isEmpty()) {
-            return;
-        }
-        User user = userOption.get();
-
-        if (!user.hasGuild()) {
-            return;
-        }
-        Guild guild = user.getGuild().get();
-
-        guild.getHome().peek(event::setRespawnLocation);
+    public void onRespawn(PlayerRespawnEvent event) {
+        this.userManager.findByPlayer(event.getPlayer())
+                .flatMap(User::getGuild)
+                .flatMap(Guild::getHome)
+                .peek(event::setRespawnLocation);
     }
 }
