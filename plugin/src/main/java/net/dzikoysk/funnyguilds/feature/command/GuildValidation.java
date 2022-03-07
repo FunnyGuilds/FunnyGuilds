9b61c0de2902bd3b7a2f760204453edcec13e07a
Peridot <peridot491@protonmail.com>
2022-03-07 11:22:39 +0100

    We can do this just in this way

--- plugin/src/main/java/net/dzikoysk/funnyguilds/feature/command/GuildValidation.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/feature/command/GuildValidation.java
@@ -2,10 +2,7 @@ package net.dzikoysk.funnyguilds.feature.command;
 
 import net.dzikoysk.funnycommands.resources.ValidationException;
 import net.dzikoysk.funnyguilds.FunnyGuilds;
-import net.dzikoysk.funnyguilds.config.MessageConfiguration;
-import net.dzikoysk.funnyguilds.config.PluginConfiguration;
 import net.dzikoysk.funnyguilds.guild.Guild;
-import net.dzikoysk.funnyguilds.guild.GuildManager;
 import org.apache.commons.lang3.StringUtils;
 
 public final class GuildValidation {
@@ -13,20 +10,8 @@ public final class GuildValidation {
     private GuildValidation() {}
 
     public static Guild requireGuildByTag(String tag) {
-        FunnyGuilds plugin = FunnyGuilds.getInstance();
-
-        PluginConfiguration pluginConfiguration = plugin.getPluginConfiguration();
-        MessageConfiguration messageConfiguration = plugin.getMessageConfiguration();
-
-        GuildManager guildManager = plugin.getGuildManager();
-
-        if (!pluginConfiguration.guildTagKeepCase) {
-            tag = pluginConfiguration.guildTagUppercase ? tag.toUpperCase() : tag.toLowerCase();
-        }
-
-        String finalTag = tag;
-        return guildManager.findByTag(tag).orThrow(() -> {
-            throw new ValidationException(StringUtils.replace(messageConfiguration.generalGuildNotExists, "{TAG}", finalTag));
+        return FunnyGuilds.getInstance().getGuildManager().findByTag(tag, true).orThrow(() -> {
+            throw new ValidationException(StringUtils.replace(FunnyGuilds.getInstance().getMessageConfiguration().generalGuildNotExists, "{TAG}", tag));
         });
     }
 
