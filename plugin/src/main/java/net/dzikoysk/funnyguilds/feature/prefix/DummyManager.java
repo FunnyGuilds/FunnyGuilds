47b91f1bc42d584c9d7659103ef8558e547d2f96
Peridot <peridot491@protonmail.com>
2022-03-15 10:54:27 +0100

    Cleanup

--- plugin/src/main/java/net/dzikoysk/funnyguilds/feature/prefix/DummyManager.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/feature/prefix/DummyManager.java
@@ -4,21 +4,24 @@ import net.dzikoysk.funnyguilds.FunnyGuilds;
 import net.dzikoysk.funnyguilds.user.User;
 import net.dzikoysk.funnyguilds.user.UserCache;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import panda.std.stream.PandaStream;
 
 public class DummyManager {
 
     public static void updatePlayers() {
         PandaStream.of(Bukkit.getOnlinePlayers())
-                .flatMap(onlinePlayer -> FunnyGuilds.getInstance().getUserManager().findByUuid(onlinePlayer.getUniqueId()))
+                .map(Player::getUniqueId)
+                .flatMap(FunnyGuilds.getInstance().getUserManager()::findByUuid)
                 .forEach(DummyManager::updateScore);
     }
 
     public static void updateScore(User user) {
         PandaStream.of(Bukkit.getOnlinePlayers())
-                .flatMap(onlinePlayer -> FunnyGuilds.getInstance().getUserManager().findByUuid(onlinePlayer.getUniqueId())
-                        .map(User::getCache)
-                        .map(UserCache::getDummy))
+                .map(Player::getUniqueId)
+                .flatMap(FunnyGuilds.getInstance().getUserManager()::findByUuid)
+                .map(User::getCache)
+                .map(UserCache::getDummy)
                 .forEach(dummy -> dummy.updateScore(user));
     }
 }
