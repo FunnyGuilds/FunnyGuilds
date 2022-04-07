cde26258d881a9f571c3ed3610a3525ae58116d6
Peridot <peridot491@protonmail.com>
2022-04-08 00:43:56 +0200

    Remove timeProperty

--- plugin/src/main/java/net/dzikoysk/funnyguilds/feature/placeholders/SimplePlaceholders.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/feature/placeholders/SimplePlaceholders.java
@@ -1,21 +1,6 @@
 package net.dzikoysk.funnyguilds.feature.placeholders;
 
-import java.time.OffsetDateTime;
-import java.util.Objects;
-import net.dzikoysk.funnyguilds.feature.placeholders.resolver.MonoResolver;
-import net.dzikoysk.funnyguilds.shared.bukkit.ChatUtils;
-
 public class SimplePlaceholders<T> extends Placeholders<T, SimplePlaceholders<T>> {
-
-    public SimplePlaceholders<T> timeProperty(String name, MonoResolver<OffsetDateTime> timeResolver) {
-        return this.property(name, (data) -> {
-            if (!(data instanceof OffsetDateTime)) {
-                return "";
-            }
-            OffsetDateTime time = (OffsetDateTime) data;
-            return ChatUtils.appendDigit(Objects.toString(timeResolver.resolve(time)));
-        });
-    }
 
     @Override
     public SimplePlaceholders<T> create() {
