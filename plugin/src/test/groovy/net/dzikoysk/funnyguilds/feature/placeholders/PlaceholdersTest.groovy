8310adced1a72f54500d5c53496a2bc57a3b09fb
Peridot <peridot491@protonmail.com>
2022-04-09 23:59:32 +0200

    Fix tests

--- plugin/src/test/groovy/net/dzikoysk/funnyguilds/feature/placeholders/PlaceholdersTest.groovy
+++ plugin/src/test/groovy/net/dzikoysk/funnyguilds/feature/placeholders/PlaceholdersTest.groovy
@@ -2,11 +2,17 @@ package net.dzikoysk.funnyguilds.feature.placeholders
 
 import groovy.transform.CompileStatic
 import net.dzikoysk.funnyguilds.FunnyGuildsSpec
+import net.dzikoysk.funnyguilds.guild.Guild
+import net.dzikoysk.funnyguilds.guild.placeholders.GuildPlaceholdersService
+import net.dzikoysk.funnyguilds.shared.bukkit.ChatUtils
+import net.dzikoysk.funnyguilds.user.FakeUserProfile
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import panda.std.Pair
 
+import static org.junit.jupiter.api.Assertions.assertEquals
 import static org.mockito.ArgumentMatchers.any
 import static org.mockito.Mockito.mock
 
@@ -29,17 +35,17 @@ class PlaceholdersTest extends FunnyGuildsSpec {
 
     @Test
     void 'test ONLINE placeholder'() {
-        /*def text = "§7offline - <online>online</online> - offline - <online>online</online> - offline"
-        def formatter = placeholdersService.onlinePlaceholders.toFormatter(ChatUtils.getLastColorBefore(text, "<online>"))
+        def text = "§7offline - <online>online</online> - offline - <online>online</online> - offline"
+        def formatter = DefaultPlaceholdersService.ONLINE.toFormatter(ChatUtils.getLastColorBefore(text, "<online>"))
 
         text = formatter.format text
 
-        assertEquals '§7offline - §aonline§7 - offline - §aonline§7 - offline', text*/
+        assertEquals '§7offline - §aonline§7 - offline - §aonline§7 - offline', text
     }
 
     @Test
     void 'test GUILD_MEMBERS_COLOR_CONTEXT placeholder'() {
-        /*def guild = guildManager.addGuild(new Guild('guild', 'TEST'))
+        def guild = guildManager.addGuild(new Guild('guild', 'TEST'))
         def user1 = userManager.createFake(UUID.randomUUID(), 'user1', FakeUserProfile.offline())
         def user2 = userManager.createFake(UUID.randomUUID(), 'user2', FakeUserProfile.online())
         def user3 = userManager.createFake(UUID.randomUUID(), 'user3', FakeUserProfile.offline())
@@ -49,22 +55,22 @@ class PlaceholdersTest extends FunnyGuildsSpec {
         guild.addMember(user3)
 
         def text1 = "§7{MEMBERS}"
-        text1 = placeholdersService.guildMembersPlaceholders
+        text1 = GuildPlaceholdersService.GUILD_MEMBERS_COLOR_CONTEXT
                 .formatVariables(text1, Pair.of(ChatUtils.getLastColorBefore(text1, "{MEMBERS}"), guild))
 
         assertEquals '§7user1, §auser2§7, user3', text1
 
         def text2 = "§c{MEMBERS}"
-        text2 = placeholdersService.guildMembersPlaceholders
+        text2 = GuildPlaceholdersService.GUILD_MEMBERS_COLOR_CONTEXT
                 .formatVariables(text2, Pair.of(ChatUtils.getLastColorBefore(text2, "{MEMBERS}"), guild))
 
         assertEquals '§cuser1, §auser2§c, user3', text2
 
         def text3 = "§a{MEMBERS}"
-        text3 = placeholdersService.guildMembersPlaceholders
+        text3 = GuildPlaceholdersService.GUILD_MEMBERS_COLOR_CONTEXT
                 .formatVariables(text3, Pair.of(ChatUtils.getLastColorBefore(text3, "{MEMBERS}"), guild))
 
-        assertEquals '§auser1, §auser2§a, user3', text3*/
+        assertEquals '§auser1, §auser2§a, user3', text3
     }
 
 }
