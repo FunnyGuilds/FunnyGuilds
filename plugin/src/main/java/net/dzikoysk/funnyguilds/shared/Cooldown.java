8cab8d09cec60fddb33c15b9990d1ffd1a20cb4a
Peridot <peridot491@protonmail.com>
2022-04-23 20:11:28 +0200

    Cool

--- plugin/src/main/java/net/dzikoysk/funnyguilds/shared/Cooldown.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/shared/Cooldown.java
@@ -1,21 +1,23 @@
 package net.dzikoysk.funnyguilds.shared;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Map;
 import java.util.WeakHashMap;
 import java.util.concurrent.TimeUnit;
 
 public final class Cooldown<T> {
 
-    private final Map<T, Long> cooldowns = new WeakHashMap<>(32);
+    private final Map<T, Instant> cooldowns = new WeakHashMap<>(32);
 
     public boolean isOnCooldown(T key) {
-        Long cooldown = cooldowns.get(key);
+        Instant cooldown = cooldowns.get(key);
 
         if (cooldown == null) {
             return false;
         }
 
-        if (cooldown > System.currentTimeMillis()) {
+        if (cooldown.isAfter(Instant.now())) {
             return true;
         }
 
@@ -23,16 +25,21 @@ public final class Cooldown<T> {
         return false;
     }
 
-    public void putOnCooldown(T key, TimeUnit unit, long duration) {
-        this.cooldowns.put(key, System.currentTimeMillis() + unit.toMillis(duration));
+    public void putOnCooldown(T key, Duration duration) {
+        this.cooldowns.put(key, Instant.now().plus(duration));
     }
 
     public void putOnCooldown(T key, long cooldown) {
-        this.cooldowns.put(key, System.currentTimeMillis() + cooldown);
+        this.putOnCooldown(key, Duration.ofMillis(cooldown));
     }
 
-    public boolean cooldown(T key, TimeUnit unit, long duration) {
-        return this.cooldown(key, unit.toMillis(duration));
+    public boolean cooldown(T key, Duration cooldown) {
+        if (isOnCooldown(key)) {
+            return true;
+        }
+
+        this.putOnCooldown(key, cooldown);
+        return false;
     }
 
     public boolean cooldown(T key, long cooldown) {
@@ -42,6 +49,10 @@ public final class Cooldown<T> {
 
         this.putOnCooldown(key, cooldown);
         return false;
+    }
+
+    public boolean cooldown(T key, TimeUnit unit, long duration) {
+        return this.cooldown(key, unit.toMillis(duration));
     }
 
 }
