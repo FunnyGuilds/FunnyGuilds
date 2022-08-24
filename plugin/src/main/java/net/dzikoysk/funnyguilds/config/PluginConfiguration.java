374345043cd606ee655418f2a56aced3fb3d46c2
Astahiell <84834748+Astahiell@users.noreply.github.com>
2022-08-24 11:00:02 +0200

    GH-2111 Add /statsreset command (Resolves #2111) (#2123)
    * GH-2111 Add statsreset command
    
    * Changes
    
    * Improve message
    
    * fix imports
    
    * add missing space
    
    * Change config release to 4.10.3
    
    Co-authored-by: Peridot <peridot491@protonmail.com>

--- plugin/src/main/java/net/dzikoysk/funnyguilds/config/PluginConfiguration.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/config/PluginConfiguration.java
@@ -56,7 +56,7 @@ import panda.std.Option;
 @Header("~-~-~-~-~-~-~-~-~-~-~-~~-~-~-~~ #")
 @Header("                                #")
 @Header("          FunnyGuilds           #")
-@Header("         4.10.2 Snowdrop        #")
+@Header("         4.10.3 Snowdrop        #")
 @Header("                                #")
 @Header("~-~-~-~-~-~-~-~-~-~-~-~~-~-~-~~ #")
 @Header("FunnyGuilds wspiera PlaceholderAPI, lista dodawanych placeholderów znajduje się tutaj:")
@@ -1114,6 +1114,11 @@ public class PluginConfiguration extends OkaeriConfig {
     @Comment("Lista przedmiotów wymaganych do resetu rankingu")
     @CustomKey("rank-reset-needed-items")
     public List<ItemStack> rankResetItems = ItemUtils.parseItems("1 diamond");
+
+    @Comment("")
+    @Comment("Lista przedmiotów wymaganych do resetu statystyk")
+    @CustomKey("stats-reset-needed-items")
+    public List<ItemStack> statsResetItems = ItemUtils.parseItems("1 diamond");
 
     @Comment("")
     @Comment("Czy przy szukaniu danych o graczu ma byc pomijana wielkość znaków jego nicku")
---
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/feature/command/user/StatsResetCommand.java
@@ -0,0 +1,62 @@
+package net.dzikoysk.funnyguilds.feature.command.user;
+
+import java.util.List;
+import net.dzikoysk.funnycommands.stereotypes.FunnyCommand;
+import net.dzikoysk.funnycommands.stereotypes.FunnyComponent;
+import net.dzikoysk.funnyguilds.feature.command.AbstractFunnyCommand;
+import net.dzikoysk.funnyguilds.shared.FunnyFormatter;
+import net.dzikoysk.funnyguilds.shared.bukkit.ItemUtils;
+import net.dzikoysk.funnyguilds.user.User;
+import net.dzikoysk.funnyguilds.user.UserRank;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+@FunnyComponent
+public final class StatsResetCommand extends AbstractFunnyCommand {
+
+    @FunnyCommand(
+            name = "${user.stats-reset.name}",
+            description = "${user.stats-reset.description}",
+            aliases = "${user.stats-reset.aliases}",
+            permission = "funnyguilds.statsreset",
+            acceptsExceeded = true,
+            playerOnly = true
+    )
+    public void execute(Player player, User user) {
+        List<ItemStack> requiredItems = this.config.statsResetItems;
+        if (!ItemUtils.playerHasEnoughItems(player, requiredItems, this.messages.statsResetItems)) {
+            return;
+        }
+
+        UserRank rank = user.getRank();
+
+        int lastPoints = rank.getPoints();
+        int lastKills = rank.getKills();
+        int lastDeaths = rank.getDeaths();
+        int lastAssists = rank.getAssists();
+        int lastLogouts = rank.getLogouts();
+
+        rank.setPoints(this.config.rankStart);
+        rank.setKills(0);
+        rank.setDeaths(0);
+        rank.setAssists(0);
+        rank.setLogouts(0);
+
+        player.getInventory().removeItem(ItemUtils.toArray(requiredItems));
+
+        FunnyFormatter formatter = new FunnyFormatter()
+                .register("{LAST-PINTS}", lastPoints)
+                .register("{CURRENT-POINTS}", rank.getPoints())
+                .register("{LAST-DEATHS}", lastDeaths)
+                .register("{CURRENT-DEATHS", rank.getDeaths())
+                .register("{LAST-KILLS}", lastKills)
+                .register("{CURRENT-KILLS}", rank.getKills())
+                .register("{LAST-ASSISTS}", lastAssists)
+                .register("{CURRENT-ASSISTS}", rank.getAssists())
+                .register("{LAST-LOGOUTS}", lastLogouts)
+                .register("{CURRENT-LOGOUTS}", rank.getLogouts());
+
+        this.messages.statsResetMessage.forEach(message -> user.sendMessage(formatter.format(message)));
+    }
+
+}
