2f6087e8e28139cb8531dfa05559e192e91e66f8
Peridot <peridot491@protonmail.com>
2022-09-03 02:07:32 +0200

    Fix AntyLogout plugins support (#2141)

--- plugin/src/main/java/net/dzikoysk/funnyguilds/listener/PlayerQuit.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/listener/PlayerQuit.java
@@ -6,23 +6,24 @@ import net.dzikoysk.funnyguilds.event.rank.LogoutsChangeEvent;
 import net.dzikoysk.funnyguilds.user.UserCache;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerQuit extends AbstractFunnyListener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onKick(PlayerKickEvent event) {
         this.handleQuit(event.getPlayer());
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onQuit(PlayerQuitEvent event) {
         this.handleQuit(event.getPlayer());
     }
 
     private void handleQuit(Player player) {
-        this.userManager.findByPlayer(player).peek(user -> {
+        this.userManager.findByUuid(player.getUniqueId()).peek(user -> {
             UserCache cache = user.getCache();
 
             if (cache.getDamageCache().isInCombat()) {
