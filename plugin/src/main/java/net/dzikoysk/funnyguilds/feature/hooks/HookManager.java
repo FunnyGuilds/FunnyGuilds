cad04a0c39782c8a870ed1c3aa4ba09f7f2ea500
Peridot <peridot491@protonmail.com>
2022-12-25 22:29:41 +0100

    chamges

--- plugin/src/main/java/net/dzikoysk/funnyguilds/feature/hooks/HookManager.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/feature/hooks/HookManager.java
@@ -75,10 +75,20 @@ public class HookManager {
                 .subscribe(hook -> PLACEHOLDER_API = hook);
 
         this.<HologramsHook>setupHook("HolographicDisplays", true, pluginName -> new HolographicDisplaysHook(pluginName, this.plugin), true)
-                .subscribe(hdHook -> HOLOGRAMS = hdHook);
+                .subscribe(hook -> {
+                    if (HOLOGRAMS.isDefined()) {
+                        return;
+                    }
+                    HOLOGRAMS = hook;
+                });
 
         this.<HologramsHook>setupHook("DecentHolograms", true, pluginName -> new DecentHologramsHook(pluginName, this.plugin), true)
-                .subscribe(dhHook -> HOLOGRAMS = dhHook);
+                .subscribe(hook -> {
+                    if (HOLOGRAMS.isDefined()) {
+                        return;
+                    }
+                    HOLOGRAMS = hook;
+                });
     }
 
     public <T extends PluginHook> Completable<Option<T>> setupHook(String pluginName, boolean requireEnabled,
