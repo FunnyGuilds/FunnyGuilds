de0f19bca97095381136e9a131ad075efe8eab7f
Peridot <peridot491@protonmail.com>
2022-12-27 00:20:34 +0100

    yes

--- plugin/src/main/java/net/dzikoysk/funnyguilds/shared/bukkit/ChatUtils.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/shared/bukkit/ChatUtils.java
@@ -15,18 +15,19 @@ public final class ChatUtils {
     }
 
     public static final Pattern DECOLOR_PATTERN = Pattern.compile("(?:\u00a7)([0-9A-Fa-fK-Ok-oRXrx][^\u00a7]*)");
+    public static final String DECOLOR_REPLACEMENT = "&$1";
 
     private static final Pattern HEX_TO_LEGACY_PATTERN = Pattern.compile("&#([0-9A-Fa-f]{1})([0-9A-Fa-f]{1})([0-9A-Fa-f]{1})([0-9A-Fa-f]{1})([0-9A-Fa-f]{1})([0-9A-Fa-f]{1})");
-    private static final Pattern LEGACY_TO_HEX_PATTERN = Pattern.compile("&[xX]&([0-9A-Fa-f]{1})&([0-9A-Fa-f]{1})&([0-9A-Fa-f]{1})&([0-9A-Fa-f]{1})&([0-9A-Fa-f]{1})&([0-9A-Fa-f]{1})");
+    private static final String LEGACY_COLOR_REPLACEMENT = "&x&$1&$2&$3&$4&$5&$6";
 
-    private static final String HEX_COLOR_PATTERN = "&#$1$2$3$4$5$6";
-    private static final String LEGACY_COLOR_PATTERN = "&x&$1&$2&$3&$4&$5&$6";
+    private static final Pattern LEGACY_TO_HEX_PATTERN = Pattern.compile("&[xX]&([0-9A-Fa-f]{1})&([0-9A-Fa-f]{1})&([0-9A-Fa-f]{1})&([0-9A-Fa-f]{1})&([0-9A-Fa-f]{1})&([0-9A-Fa-f]{1})");
+    private static final String HEX_COLOR_REPLACEMENT = "&#$1$2$3$4$5$6";
 
     public static String colored(String message) {
         if (message == null) {
             return "";
         }
-        message = HEX_TO_LEGACY_PATTERN.matcher(message).replaceAll(LEGACY_COLOR_PATTERN);
+        message = HEX_TO_LEGACY_PATTERN.matcher(message).replaceAll(LEGACY_COLOR_REPLACEMENT);
         return ChatColor.translateAlternateColorCodes('&', message);
     }
 
@@ -38,8 +39,8 @@ public final class ChatUtils {
         if (coloredString == null) {
             return null;
         }
-        String rawString = DECOLOR_PATTERN.matcher(coloredString).replaceAll("&$1");
-        rawString = LEGACY_TO_HEX_PATTERN.matcher(rawString).replaceAll(HEX_COLOR_PATTERN);
+        String rawString = DECOLOR_PATTERN.matcher(coloredString).replaceAll(DECOLOR_REPLACEMENT);
+        rawString = LEGACY_TO_HEX_PATTERN.matcher(rawString).replaceAll(HEX_COLOR_REPLACEMENT);
         return rawString;
     }
 
