61f54c495ffd2b6e1256edbdf24e56b00acf5f3a
Peridot <peridot491@protonmail.com>
2022-12-27 00:03:10 +0100

    Simple HEX

--- plugin/src/main/java/net/dzikoysk/funnyguilds/shared/bukkit/ChatUtils.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/shared/bukkit/ChatUtils.java
@@ -14,10 +14,17 @@ public final class ChatUtils {
     private ChatUtils() {
     }
 
+    private static final Pattern HEX_PATTERN = Pattern.compile("&#([0-9A-Fa-f]{1})([0-9A-Fa-f]{1})([0-9A-Fa-f]{1})([0-9A-Fa-f]{1})([0-9A-Fa-f]{1})([0-9A-Fa-f]{1})");
+    private static final Pattern REVERSE_HEX_PATTERN = Pattern.compile("&[xX]&([0-9A-Fa-f]{1})&([0-9A-Fa-f]{1})&([0-9A-Fa-f]{1})&([0-9A-Fa-f]{1})&([0-9A-Fa-f]{1})&([0-9A-Fa-f]{1})");
+
     public static final Pattern DECOLOR_PATTERN = Pattern.compile("(?:\u00a7)([0-9A-Fa-fK-Ok-oRXrx][^\u00a7]*)");
 
     public static String colored(String message) {
-        return message != null ? ChatColor.translateAlternateColorCodes('&', message) : "";
+        if (message == null) {
+            return "";
+        }
+        message = HEX_PATTERN.matcher(message).replaceAll("&x&$1&$2&$3&$4&$5&$6");
+        return ChatColor.translateAlternateColorCodes('&', message);
     }
 
     public static List<String> colored(List<String> messages) {
@@ -25,7 +32,12 @@ public final class ChatUtils {
     }
 
     public static String decolor(String coloredString) {
-        return DECOLOR_PATTERN.matcher(coloredString).replaceAll("&$1");
+        if (coloredString == null) {
+            return null;
+        }
+        String rawString = DECOLOR_PATTERN.matcher(coloredString).replaceAll("&$1");
+        rawString = REVERSE_HEX_PATTERN.matcher(rawString).replaceAll("&#$1$2$3$4$5$6");
+        return rawString;
     }
 
     public static String getLastColorBefore(String text, String before) {
