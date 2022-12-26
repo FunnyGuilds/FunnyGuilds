0e0a144302ce93bb11c8cfec0c6c455387b3240b
Peridot <peridot491@protonmail.com>
2022-12-27 00:53:21 +0100

    GH-2212 Support DecentHolograms (Resolves #2068)
    * Add decentholograms dependency
    
    * add softdepend to dont forgor :skull:
    
    * cringe
    
    * try code
    
    * fixes
    
    * chamges
    
    * finally everything works
    
    * chamges
    
    * chamges
    
    * now???

--- plugin/src/main/java/net/dzikoysk/funnyguilds/feature/hooks/decentholograms/DecentHologramsHook.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/feature/hooks/decentholograms/DecentHologramsHook.java
@@ -1,6 +1,10 @@
-package net.dzikoysk.funnyguilds.feature.hooks.holographicdisplays;
+package net.dzikoysk.funnyguilds.feature.hooks.decentholograms;
 
-import eu.okaeri.commons.bukkit.holographicdisplays.Holo;
+import eu.decentsoftware.holograms.api.DHAPI;
+import eu.decentsoftware.holograms.api.holograms.Hologram;
+import eu.decentsoftware.holograms.api.utils.items.HologramItem;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import net.dzikoysk.funnyguilds.FunnyGuilds;
@@ -22,13 +26,13 @@ import org.jetbrains.annotations.NotNull;
 import panda.std.Option;
 import panda.std.stream.PandaStream;
 
-public final class HolographicDisplaysHook extends HologramsHook implements Listener {
+public class DecentHologramsHook extends HologramsHook implements Listener {
 
     private final FunnyGuilds plugin;
     private final PluginConfiguration config;
-    private final Map<Guild, Holo> holograms = new ConcurrentHashMap<>();
+    private final Map<Guild, Hologram> holograms = new ConcurrentHashMap<>();
 
-    public HolographicDisplaysHook(String name, FunnyGuilds plugin) {
+    public DecentHologramsHook(String name, FunnyGuilds plugin) {
         super(name);
         this.plugin = plugin;
         this.config = plugin.getPluginConfiguration();
@@ -47,30 +51,41 @@ public final class HolographicDisplaysHook extends HologramsHook implements List
 
     @Override
     public void update(@NotNull Guild guild) {
+        this.update(guild, false);
+    }
+
+    public void update(@NotNull Guild guild, boolean updateLocation) {
         HologramConfiguration holoConfig = this.config.heart.hologram;
         if (!holoConfig.enabled) {
             return;
         }
 
-        Option<Location> guildCenter = guild.getCenter();
-        if (guildCenter.isEmpty()) {
+        Option<Location> guildCenterOption = guild.getCenter();
+        if (guildCenterOption.isEmpty()) {
             return;
         }
+        Location holoCenter = guildCenterOption.get()
+                .add(holoConfig.locationCorrection);
 
-        Holo holo = this.holograms.computeIfAbsent(guild, (g) -> Holo.create(this.plugin));
-        holo.location(guildCenter.map(it -> it.add(holoConfig.locationCorrection)).get());
-        holo.clear();
+        Hologram holo = this.holograms.computeIfAbsent(
+                guild,
+                (g) -> DHAPI.createHologram(prepareHologramName(guild), holoCenter, false)
+        );
 
-        if (holoConfig.item != Material.AIR) {
-            holo.appendItem(new ItemStack(holoConfig.item));
+        if (updateLocation) {
+            DHAPI.moveHologram(holo, holoCenter);
         }
 
-        holo.appendTexts(PandaStream.of(holoConfig.displayedLines)
+        List<String> lines = new ArrayList<>();
+        if (holoConfig.item != Material.AIR) {
+            // A little hacky, but prevents from hologram blinking
+            lines.add("#ICON:" + HologramItem.fromItemStack(new ItemStack(holoConfig.item)).getContent());
+        }
+        lines.addAll(PandaStream.of(holoConfig.displayedLines)
                 .map(line -> this.plugin.getGuildPlaceholdersService().format(line, guild))
                 .map(ChatUtils::colored)
                 .toList());
-
-        holo.update();
+        DHAPI.setHologramLines(holo, lines);
     }
 
     @Override
@@ -79,17 +94,17 @@ public final class HolographicDisplaysHook extends HologramsHook implements List
             return;
         }
 
-        this.holograms.values().forEach(Holo::delete);
+        this.holograms.values().forEach(Hologram::destroy);
         this.holograms.clear();
     }
 
     @EventHandler
     public void handleGuildDelete(GuildDeleteEvent event) {
-        Holo holo = this.holograms.remove(event.getGuild());
+        Hologram holo = this.holograms.remove(event.getGuild());
         if (holo == null) {
             return;
         }
-        holo.delete();
+        holo.destroy();
     }
 
     @EventHandler
@@ -99,7 +114,11 @@ public final class HolographicDisplaysHook extends HologramsHook implements List
 
     @EventHandler
     public void handleGuildMove(GuildMoveEvent event) {
-        this.update(event.getGuild());
+        this.update(event.getGuild(), true);
+    }
+
+    private static String prepareHologramName(Guild guild) {
+        return "funnyguilds:guild:" + guild.getName();
     }
 
 }
