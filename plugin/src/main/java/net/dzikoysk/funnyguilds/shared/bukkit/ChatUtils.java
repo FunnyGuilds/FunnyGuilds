822b5100d0e2a158855ef27419532ca0b7d086d5
domirusz24 <DomiRusz24@gmail.com>
2022-12-27 00:25:21 +0100

    Implemented FunnyGuilds/FunnyGuilds#2178

--- plugin/src/main/java/net/dzikoysk/funnyguilds/shared/bukkit/ChatUtils.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/shared/bukkit/ChatUtils.java
@@ -1,6 +1,7 @@
 package net.dzikoysk.funnyguilds.shared.bukkit;
 
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import net.dzikoysk.funnyguilds.shared.FunnyStringUtils;
 import org.bukkit.Bukkit;
@@ -17,7 +18,25 @@ public final class ChatUtils {
     public static final Pattern DECOLOR_PATTERN = Pattern.compile("(?:\u00a7)([0-9A-Fa-fK-Ok-oRXrx][^\u00a7]*)");
 
     public static String colored(String message) {
-        return message != null ? ChatColor.translateAlternateColorCodes('&', message) : "";
+        return message != null ? hexColored("&#", ChatColor.translateAlternateColorCodes('&', message)) : "";
+    }
+
+    public static String hexColored(String startTag, String message) {
+        final Pattern hexPattern = Pattern.compile(startTag + "([A-Fa-f0-9]{6})");
+        Matcher matcher = hexPattern.matcher(message);
+        StringBuffer buffer = new StringBuffer(message.length() + 4 * 8);
+
+        final char colorChar = ChatColor.COLOR_CHAR;
+
+        while (matcher.find()) {
+            String group = matcher.group(1);
+            matcher.appendReplacement(buffer, colorChar + "x"
+                    + colorChar + group.charAt(0) + colorChar + group.charAt(1)
+                    + colorChar + group.charAt(2) + colorChar + group.charAt(3)
+                    + colorChar + group.charAt(4) + colorChar + group.charAt(5)
+            );
+        }
+        return matcher.appendTail(buffer).toString();
     }
 
     public static List<String> colored(List<String> messages) {
