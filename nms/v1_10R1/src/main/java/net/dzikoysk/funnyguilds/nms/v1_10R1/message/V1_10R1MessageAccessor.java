8849cd9ded31fe887a926b26fbaf8c4fb8792218
Peridot <peridot491@protonmail.com>
2023-01-02 18:12:39 +0100

    GH-2224 Use less NMS (#2224)
    * Use less nms for actionbar/title sending and default methods in interfaces exists
    
    * use API to obtain Player ping

--- nms/v1_10R1/src/main/java/net/dzikoysk/funnyguilds/nms/v1_10R1/message/V1_10R1MessageAccessor.java
+++ nms/v1_10R1/src/main/java/net/dzikoysk/funnyguilds/nms/v1_10R1/message/V1_10R1MessageAccessor.java
@@ -1,10 +1,9 @@
 package net.dzikoysk.funnyguilds.nms.v1_10R1.message;
 
-import java.util.Collection;
 import net.dzikoysk.funnyguilds.nms.api.message.MessageAccessor;
 import net.dzikoysk.funnyguilds.nms.api.message.TitleMessage;
-import net.minecraft.server.v1_10_R1.IChatBaseComponent.ChatSerializer;
-import net.minecraft.server.v1_10_R1.PacketPlayOutChat;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.minecraft.server.v1_10_R1.PacketPlayOutTitle;
 import net.minecraft.server.v1_10_R1.PacketPlayOutTitle.EnumTitleAction;
 import org.bukkit.craftbukkit.v1_10_R1.entity.CraftPlayer;
@@ -36,25 +35,10 @@ public class V1_10R1MessageAccessor implements MessageAccessor {
     }
 
     @Override
-    public void sendTitleMessage(TitleMessage titleMessage, Collection<? extends Player> players) {
-        this.sendTitleMessage(titleMessage, players.toArray(new Player[0]));
-    }
-
-    @Override
     public void sendActionBarMessage(String text, Player... players) {
-        PacketPlayOutChat actionBarPacket = new PacketPlayOutChat(
-                ChatSerializer.a("{\"text\":\"" + text + "\"}"),
-                (byte) 2
-        );
-
         for (Player player : players) {
-            ((CraftPlayer) player).getHandle().playerConnection.sendPacket(actionBarPacket);
+            player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(text));
         }
-    }
-
-    @Override
-    public void sendActionBarMessage(String text, Collection<? extends Player> players) {
-        this.sendActionBarMessage(text, players.toArray(new Player[0]));
     }
 
 }
--- nms/v1_19R1/src/main/java/net/dzikoysk/funnyguilds/nms/v1_19R1/statistics/V1_19R1StatisticsAccessor.java
+++ nms/v1_19R1/src/main/java/net/dzikoysk/funnyguilds/nms/v1_19R1/statistics/V1_19R1StatisticsAccessor.java
@@ -1,10 +1,8 @@
 package net.dzikoysk.funnyguilds.nms.v1_19R1.statistics;
 
-import com.google.common.base.Preconditions;
 import net.dzikoysk.funnyguilds.nms.api.statistics.StatisticsAccessor;
 import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_19_R1.CraftServer;
-import org.bukkit.craftbukkit.v1_19_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 public class V1_19R1StatisticsAccessor implements StatisticsAccessor {
@@ -21,8 +19,7 @@ public class V1_19R1StatisticsAccessor implements StatisticsAccessor {
 
     @Override
     public int getPlayerPing(Player player) {
-        Preconditions.checkNotNull(player, "player can't be null!");
-        return ((CraftPlayer) player).getHandle().e;
+        return player.getPing();
     }
 
 }
