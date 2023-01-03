c7670c115b093e2aa6c145d6b6dce8ca73c07da9
Peridot <peridot491@protonmail.com>
2023-01-03 19:39:29 +0100

    use okaeri-configs move lol

--- plugin/src/main/java/net/dzikoysk/funnyguilds/config/migration/P0008_Migrate_old_heart_center_configuration.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/config/migration/P0008_Migrate_old_heart_center_configuration.java
@@ -1,8 +1,6 @@
 package net.dzikoysk.funnyguilds.config.migration;
 
-import eu.okaeri.configs.migrate.ConfigMigration;
 import eu.okaeri.configs.migrate.builtin.NamedMigration;
-import java.util.function.Function;
 import net.dzikoysk.funnyguilds.config.sections.HeartConfiguration.Center.Height;
 
 import static eu.okaeri.configs.migrate.ConfigMigrationDsl.move;
@@ -12,7 +10,7 @@ public class P0008_Migrate_old_heart_center_configuration extends NamedMigration
     public P0008_Migrate_old_heart_center_configuration() {
         super(
                 "Migrate old heart center configuration to center subconfig",
-                moveAndTransform("heart-configuration.use-player-position-for-center-y", "heart-configuration.center.height", oldValue -> {
+                move("heart-configuration.use-player-position-for-center-y", "heart-configuration.center.height", oldValue -> {
                     if (oldValue instanceof Boolean && (Boolean) oldValue) {
                         return Height.PLAYER;
                     }
@@ -20,19 +18,6 @@ public class P0008_Migrate_old_heart_center_configuration extends NamedMigration
                 }),
                 move("heart-configuration.create-center-y", "heart-configuration.center.fixed-height")
         );
-    }
-
-    private static ConfigMigration moveAndTransform(String oldKey, String newKey, Function<Object, Object> transformer) {
-        return (config, view) -> {
-            if (!view.exists(oldKey)) {
-                return false;
-            }
-
-            Object oldValue = view.remove(oldKey);
-            Object targetValue = transformer.apply(oldValue);
-            view.set(newKey, targetValue);
-            return true;
-        };
     }
 
 }
