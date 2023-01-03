4994c50d25df717938d1372654ad2eda3bc0bbfa
Peridot <peridot491@protonmail.com>
2023-01-03 18:48:55 +0100

    Fix migrations

--- plugin/src/main/java/net/dzikoysk/funnyguilds/config/migration/P0008_Migrate_old_heart_center_configuration.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/config/migration/P0008_Migrate_old_heart_center_configuration.java
@@ -1,25 +1,38 @@
 package net.dzikoysk.funnyguilds.config.migration;
 
+import eu.okaeri.configs.migrate.ConfigMigration;
 import eu.okaeri.configs.migrate.builtin.NamedMigration;
+import java.util.function.Function;
 import net.dzikoysk.funnyguilds.config.sections.HeartConfiguration.Center.Height;
 
 import static eu.okaeri.configs.migrate.ConfigMigrationDsl.move;
-import static eu.okaeri.configs.migrate.ConfigMigrationDsl.update;
-import static eu.okaeri.configs.migrate.ConfigMigrationDsl.when;
 
 public class P0008_Migrate_old_heart_center_configuration extends NamedMigration {
 
     public P0008_Migrate_old_heart_center_configuration() {
         super(
                 "Migrate old heart center configuration to center subconfig",
-                when(move("heart.use-player-position-for-center-y", "heart.center.height"), update("heart.center.height", oldValue -> {
-                    if (oldValue instanceof Boolean) {
-                        return (Boolean) oldValue ? Height.PLAYER : Height.FIXED;
+                moveAndTransform("heart-configuration.use-player-position-for-center-y", "heart-configuration.center.height", oldValue -> {
+                    if (oldValue instanceof Boolean && (Boolean) oldValue) {
+                        return Height.PLAYER;
                     }
                     return Height.FIXED;
-                })),
-                move("heart.create-center-y", "heart.center.fixed-height")
+                }),
+                move("heart-configuration.create-center-y", "heart-configuration.center.fixed-height")
         );
+    }
+
+    private static ConfigMigration moveAndTransform(String oldKey, String newKey, Function<Object, Object> transformer) {
+        return (config, view) -> {
+            if (!view.exists(oldKey)) {
+                return false;
+            }
+
+            Object oldValue = view.remove(oldKey);
+            Object targetValue = transformer.apply(oldValue);
+            view.set(newKey, targetValue);
+            return true;
+        };
     }
 
 }
