7872a37a5a1dc060f89bc00ec6995301faca9377
Peridot <peridot491@protonmail.com>
2023-01-18 21:06:44 +0100

    Remove old DefaultValidation methods

--- plugin/src/main/java/net/dzikoysk/funnyguilds/feature/command/DefaultValidation.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/feature/command/DefaultValidation.java
@@ -1,9 +1,6 @@
 package net.dzikoysk.funnyguilds.feature.command;
 
-import java.util.Objects;
 import java.util.function.Function;
-import java.util.function.Supplier;
-import net.dzikoysk.funnycommands.resources.ValidationException;
 import net.dzikoysk.funnyguilds.config.message.MessageConfiguration;
 import panda.std.Option;
 import pl.peridot.yetanothermessageslibrary.message.Sendable;
@@ -14,27 +11,9 @@ public final class DefaultValidation {
     private DefaultValidation() {
     }
 
-    public static void whenNull(Object value, Supplier<Object> message) {
-        if (value == null) {
-            throw new ValidationException(Objects.toString(message.get()));
-        }
-    }
-
-    public static void whenNull(Object value, String message) {
-        if (value == null) {
-            throw new ValidationException(message);
-        }
-    }
-
     public static void whenNull(Object value, Function<MessageConfiguration, Sendable> messageSupplier, Replaceable... replacements) {
         if (value == null) {
             throw new InternalValidationException(messageSupplier, replacements);
-        }
-    }
-
-    public static void when(boolean flag, String message) {
-        if (flag) {
-            throw new ValidationException(message);
         }
     }
 
@@ -44,18 +23,8 @@ public final class DefaultValidation {
         }
     }
 
-    public static <T> T when(Option<T> option, String message) {
-        return option.orThrow(() -> new ValidationException(message));
-    }
-
     public static <T> T when(Option<T> option, Function<MessageConfiguration, Sendable> messageSupplier, Replaceable... replacements) {
         return option.orThrow(() -> new InternalValidationException(messageSupplier, replacements));
-    }
-
-    public static void when(boolean flag, Supplier<Object> message) {
-        if (flag) {
-            throw new ValidationException(Objects.toString(message.get()));
-        }
     }
 
 }
