f2cb64f5a8f90b58e7b3b72d4e55ae1411f258bf
nwseOOO <nwse@nwse.pl>
2023-03-21 17:29:12 +0100

    Update V1_19R3PacketAccessor.java

--- nms/v1_19R3/src/main/java/net/dzikoysk/funnyguilds/nms/v1_19R3/packet/V1_19R3PacketAccessor.java
+++ nms/v1_19R3/src/main/java/net/dzikoysk/funnyguilds/nms/v1_19R3/packet/V1_19R3PacketAccessor.java
@@ -9,7 +9,6 @@ import net.dzikoysk.funnyguilds.nms.v1_16R3.packet.GenericInboundChannelHandlerI
 import net.dzikoysk.funnyguilds.nms.v1_16R3.packet.GenericOutboundChannelHandlerInstaller;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.server.level.EntityPlayer;
-import net.minecraft.server.network.PlayerConnection;
 import org.bukkit.craftbukkit.v1_19_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
@@ -17,35 +16,16 @@ import java.lang.reflect.Field;
 
 public class V1_19R3PacketAccessor implements PacketAccessor {
     
-    private static final Field NETOWRK_MANAGER_FIELD;
-
     private final GenericInboundChannelHandlerInstaller<V1_19R3FunnyGuildsInboundChannelHandler> inboundChannelHandlerInstaller =
             new GenericInboundChannelHandlerInstaller<>(V1_19R3FunnyGuildsInboundChannelHandler::new);
     private final GenericOutboundChannelHandlerInstaller<V1_19R3FunnyGuildsOutboundChannelHandler> outboundChannelHandlerInstaller =
             new GenericOutboundChannelHandlerInstaller<>(V1_19R3FunnyGuildsOutboundChannelHandler::new);
     
-    static {
-        try {
-            NETOWRK_MANAGER_FIELD = PlayerConnection.class.getDeclaredField("h");
-            NETOWRK_MANAGER_FIELD.setAccessible(true);
-        }
-        catch (NoSuchFieldException exception) {
-            throw new RuntimeException("Failed to initialise V1_19R3PacketAccessor", exception);
-        }
-    }
-    
     @Override
     public FunnyGuildsInboundChannelHandler getOrInstallInboundChannelHandler(Player player) {
         EntityPlayer entityPlayer = ((CraftPlayer) player).getHandle();
-        
-        NetworkManager networkManager;
-        try {
-            networkManager = (NetworkManager) NETOWRK_MANAGER_FIELD.get(entityPlayer);
-        } catch (IllegalAccessException exception) {
-            throw new RuntimeException("Failed to initialise V1_19R3PacketAccessor", exception);
-        }
-        
-        Channel channel = networkManager.m;
+    
+        Channel channel = this.getNetworkManager(entityPlayer).m;
         ChannelPipeline pipeline = channel.pipeline();
 
         return this.inboundChannelHandlerInstaller.installChannelHandlerInPipeline(pipeline);
@@ -55,17 +35,26 @@ public class V1_19R3PacketAccessor implements PacketAccessor {
     public FunnyGuildsOutboundChannelHandler getOrInstallOutboundChannelHandler(Player player) {
         EntityPlayer entityPlayer = ((CraftPlayer) player).getHandle();
     
-        NetworkManager networkManager;
-        try {
-            networkManager = (NetworkManager) NETOWRK_MANAGER_FIELD.get(entityPlayer);
-        } catch (IllegalAccessException exception) {
-            throw new RuntimeException("Failed to initialise V1_19R3PacketAccessor", exception);
-        }
-    
-        Channel channel = networkManager.m;
+        Channel channel = this.getNetworkManager(entityPlayer).m;
         ChannelPipeline pipeline = channel.pipeline();
 
         return this.outboundChannelHandlerInstaller.installChannelHandlerOutPipeline(pipeline);
+    }
+    
+    private NetworkManager getNetworkManager(EntityPlayer entityPlayer) {
+        Field networkManagerField;
+        try {
+            networkManagerField = entityPlayer.b.getClass().getDeclaredField("h");
+            networkManagerField.setAccessible(true);
+        } catch (NoSuchFieldException exception) {
+            throw new RuntimeException("missing 'h' field in PlayerConnection", exception);
+        }
+    
+        try {
+            return (NetworkManager) networkManagerField.get(entityPlayer.b);
+        } catch (IllegalAccessException exception) {
+            throw new RuntimeException("Failed to initialise V1_19R3PacketAccessor", exception);
+        }
     }
 
 }
