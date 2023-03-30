8fb0daee4531ac0a2779d9aa2ce556742b51735c
Peridot <peridot491@protonmail.com>
2023-03-30 12:26:26 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,9 +33,6 @@ allprojects {
     }
 
     repositories {
-        /* Local */
-        mavenLocal()
-
         /* Libs */
         maven("https://maven.reposilite.com/releases")
         maven("https://storehouse.okaeri.eu/repository/maven-public")
