1a16411845af51a4c81caa260cf4bc79d68703cb
Peridot <peridot491@protonmail.com>
2023-04-05 23:03:27 +0200

    GH-2391 Fix security check when guild's heart is a block (Fixes #2018)
    * GH-2018 Fix security check when guild's heart is a block (Fix GH-2018)
    
    * simplify excluding heart
    
    * Change raycasting method
    
    * Use cache to store violations
    
    * bump reach values
    
    * Restore sneaking free-cam compensation and move reach-values to configuration
    
    * Write migration
    
    * Fix unneeded parser log
    
    * intellij cringe
    
    * intellij cringe

--- plugin/src/main/java/net/dzikoysk/funnyguilds/feature/security/cheat/SecurityReach.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/feature/security/cheat/SecurityReach.java
@@ -3,7 +3,7 @@ package net.dzikoysk.funnyguilds.feature.security.cheat;
 import dev.peri.yetanothermessageslibrary.replace.replacement.Replacement;
 import java.text.DecimalFormat;
 import net.dzikoysk.funnyguilds.FunnyGuilds;
-import net.dzikoysk.funnyguilds.config.PluginConfiguration;
+import net.dzikoysk.funnyguilds.config.sections.SecuritySystemConfiguration;
 import net.dzikoysk.funnyguilds.feature.security.SecurityUtils;
 import net.dzikoysk.funnyguilds.shared.bukkit.NmsUtils;
 import net.dzikoysk.funnyguilds.user.UserManager;
@@ -13,8 +13,6 @@ import org.bukkit.entity.Player;
 public final class SecurityReach {
 
     private static final DecimalFormat FORMAT = new DecimalFormat("##.##");
-    private static final double CREATIVE_REACH = 4.5;
-    private static final double SURVIVAL_REACH = 3.0;
     private static final double IMPORTANCE_OF_PING = 0.93;
     private static final double IMPORTANCE_OF_TPS = 10.0;
 
@@ -23,14 +21,16 @@ public final class SecurityReach {
 
     public static void on(Player player, double distance) {
         FunnyGuilds funnyGuilds = FunnyGuilds.getInstance();
-        PluginConfiguration config = funnyGuilds.getPluginConfiguration();
+        SecuritySystemConfiguration.Reach config = funnyGuilds.getPluginConfiguration().securitySystem.reach;
         UserManager userManager = funnyGuilds.getUserManager();
 
         double ping = player.getPing();
         double tpsDelayMs = (1000.0 / NmsUtils.getTpsInLastMinute() - 50.0);
-        double compensation = player.getGameMode() == GameMode.CREATIVE ? CREATIVE_REACH : SURVIVAL_REACH;
+        double compensation = player.getGameMode() == GameMode.CREATIVE
+                ? config.creativeReach
+                : config.survivalReach;
 
-        compensation += config.reachCompensation;
+        compensation += config.compensation;
         compensation += SecurityUtils.compensationMs(IMPORTANCE_OF_PING * ping);
         compensation += SecurityUtils.compensationMs(IMPORTANCE_OF_TPS * tpsDelayMs);
 
