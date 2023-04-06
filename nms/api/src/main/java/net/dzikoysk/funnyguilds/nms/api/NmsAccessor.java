d0e2f6e9980a3aee0cb66309dace384ec7bd87e1
Szczurowsky <kamilszczurowski@hotmail.com>
2023-04-06 02:15:59 +0200

    Delete useless NMS

--- nms/api/src/main/java/net/dzikoysk/funnyguilds/nms/api/NmsAccessor.java
+++ nms/api/src/main/java/net/dzikoysk/funnyguilds/nms/api/NmsAccessor.java
@@ -2,14 +2,11 @@ package net.dzikoysk.funnyguilds.nms.api;
 
 import net.dzikoysk.funnyguilds.nms.api.entity.EntityAccessor;
 import net.dzikoysk.funnyguilds.nms.api.packet.PacketAccessor;
-import net.dzikoysk.funnyguilds.nms.api.playerlist.PlayerListAccessor;
 import net.dzikoysk.funnyguilds.nms.api.statistics.StatisticsAccessor;
 
 public interface NmsAccessor {
 
     PacketAccessor getPacketAccessor();
-
-    PlayerListAccessor getPlayerListAccessor();
 
     StatisticsAccessor getStatisticsAccessor();
 
