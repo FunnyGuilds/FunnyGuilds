8da982311a9627b9db2579cc5353934bca55c440
Oskar <43253100+oskarscot@users.noreply.github.com>
2023-04-06 12:06:02 +0100

    GH-2396 Send message to all guild members, not just player in HelpRequestCommand (#2396)
    * Send message to all guild members, not just player
    
    * Pass guild

--- plugin/src/main/java/net/dzikoysk/funnyguilds/feature/command/user/HelpRequestCommand.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/feature/command/user/HelpRequestCommand.java
@@ -3,6 +3,7 @@ package net.dzikoysk.funnyguilds.feature.command.user;
 import net.dzikoysk.funnycommands.stereotypes.FunnyCommand;
 import net.dzikoysk.funnyguilds.feature.command.AbstractFunnyCommand;
 import net.dzikoysk.funnyguilds.feature.command.IsMember;
+import net.dzikoysk.funnyguilds.guild.Guild;
 import net.dzikoysk.funnyguilds.shared.Cooldown;
 import net.dzikoysk.funnyguilds.shared.FunnyFormatter;
 import net.dzikoysk.funnyguilds.user.User;
@@ -20,7 +21,7 @@ public class HelpRequestCommand extends AbstractFunnyCommand {
             acceptsExceeded = true,
             playerOnly = true
     )
-    public void execute(Player player, @IsMember User member) {
+    public void execute(Player player, @IsMember User member, Guild guild) {
         if (this.requestCooldown.cooldown(player, this.config.helpRequestCooldown)) {
             return;
         }
@@ -33,7 +34,7 @@ public class HelpRequestCommand extends AbstractFunnyCommand {
 
         this.messageService.getMessage(config -> config.guild.commands.helpRequest)
                 .with(formatter)
-                .receiver(player)
+                .receiver(guild)
                 .send();
     }
 }
