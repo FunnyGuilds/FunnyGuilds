2ae3d2abbf05524439cd93d135c2c27fee4b1c6e
Peridot <peridot491@protonmail.com>
2023-07-05 15:23:50 +0200

    Use MonoResolver

--- plugin/src/main/java/net/dzikoysk/funnyguilds/feature/placeholders/OffsetDateTimePlaceholders.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/feature/placeholders/OffsetDateTimePlaceholders.java
@@ -2,13 +2,13 @@ package net.dzikoysk.funnyguilds.feature.placeholders;
 
 import java.time.OffsetDateTime;
 import java.util.Objects;
-import java.util.function.Function;
+import net.dzikoysk.funnyguilds.feature.placeholders.resolver.MonoResolver;
 import net.dzikoysk.funnyguilds.shared.FunnyStringUtils;
 
 public class OffsetDateTimePlaceholders extends Placeholders<OffsetDateTime, OffsetDateTimePlaceholders> {
 
-    public OffsetDateTimePlaceholders timeProperty(String name, Function<OffsetDateTime, Object> timeSupplier) {
-        return this.property(name, (entity, data) -> FunnyStringUtils.appendDigit(Objects.toString(timeSupplier.apply(data))));
+    public OffsetDateTimePlaceholders timeProperty(String name, MonoResolver<OffsetDateTime> timeSupplier) {
+        return this.property(name, (entity, data) -> FunnyStringUtils.appendDigit(Objects.toString(timeSupplier.resolve(data))));
     }
 
     @Override
