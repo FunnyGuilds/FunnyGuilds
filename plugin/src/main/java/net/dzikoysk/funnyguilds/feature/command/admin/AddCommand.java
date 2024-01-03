423381c95bcee858fd3cde6eec6b75744647ca3d
PvPWorldPL <149802377+PvPWorldPL@users.noreply.github.com>
2024-01-03 12:38:19 +0100

    Update AddCommand.java

--- plugin/src/main/java/net/dzikoysk/funnyguilds/feature/command/admin/AddCommand.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/feature/command/admin/AddCommand.java
@@ -1,4 +1,4 @@
- package net.dzikoysk.funnyguilds.feature.command.admin;
+package net.dzikoysk.funnyguilds.feature.command.admin;
 
 import net.dzikoysk.funnycommands.stereotypes.FunnyCommand;
 import net.dzikoysk.funnyguilds.event.SimpleEventHandler;
@@ -19,28 +19,32 @@ public final class AddCommand extends AbstractFunnyCommand {
             name = "${admin.add.name}",
             permission = "funnyguilds.admin",
             completer = "guilds:3 online-players:3",
-            acceptsExceeded = true,            
+            acceptsExceeded = true,
     )
     public void execute(CommandSender sender, String[] args) {
         when(args.length < 1, config -> config.commands.validation.noTagGiven);
         Guild guild = GuildValidation.requireGuildByTag(args[0]);
+
         when(args.length < 2, config -> config.commands.validation.noNickGiven);
         User userToAdd = UserValidation.requireUserByName(args[1]);
         when(userToAdd.hasGuild(), config -> config.commands.validation.userHasGuild);
+
         User admin = AdminUtils.getAdminUser(sender);
         if (!SimpleEventHandler.handle(new GuildMemberJoinEvent(AdminUtils.getCause(admin), admin, guild, userToAdd))) {
             return;
         }
-     
+
         guild.addMember(userToAdd);
         userToAdd.setGuild(guild);
         this.plugin.getIndividualNameTagManager()
                 .map(manager -> new ScoreboardGlobalUpdateUserSyncTask(manager, userToAdd))
                 .peek(this.plugin::scheduleFunnyTasks);
+
         FunnyFormatter formatter = new FunnyFormatter()
                 .register("{GUILD}", guild.getName())
                 .register("{TAG}", guild.getTag())
                 .register("{PLAYER}", userToAdd.getName());
+
         this.messageService.getMessage(config -> config.guild.commands.join.joined)
                 .receiver(guild.getOwner())
                 .with(formatter)
