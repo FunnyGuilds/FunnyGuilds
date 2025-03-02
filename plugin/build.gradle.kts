0f0cb98c511be70c2547968b1e6e543ccf31ce3f
Peridot <peridot491@protonmail.com>
2025-03-02 11:54:36 +0100

    Remove origin from branch name

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -124,7 +124,18 @@ tasks.processResources {
     val grgit = grgitService.service.get().grgit
     val isCiServer = System.getenv().containsKey("CI")
     
-    val version = "${project.version}${if (isCiServer) "+${grgit.branch.current().trackingBranch.name}" else ""}"
+    val version = project.version.toString().let { version ->
+        takeIf { isCiServer }
+            ?.let {
+                grgit.branch.current().trackingBranch.name.let { branchName -> grgit.remote.list()
+                    .map { it.name }
+                    .find { remote -> branchName.startsWith(remote) }
+                    ?.let { remote -> branchName.removePrefix("$remote/") }
+                    ?: branchName
+                }.let { branchName -> "$version+$branchName" }
+            }
+            ?: version
+    }
     val commitId = grgit.head().abbreviatedId
     
     expand(
