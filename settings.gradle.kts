c893b80f297858896d0eaa87d1e829dcc85266ed
Peridot <peridot491@protonmail.com>
2025-03-02 10:57:36 +0100

    Enable build cache for gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,3 +28,11 @@ include(
     ":nms:v1_21R1",
     ":nms:v1_21_4",
 )
+
+val isCiServer = System.getenv().containsKey("CI")
+// Cache build artifacts, so expensive operations do not need to be re-computed
+buildCache {
+   local {
+       isEnabled = !isCiServer
+   }
+}
