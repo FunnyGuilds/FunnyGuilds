93bd8c9052dec56ac2e9cef0f8eee335c6842860
Piotr Karnak <github@peri.dev>
2025-11-07 10:08:25 +0100

    Update GuildPermissionController API to allow to pass specific guild

--- plugin/src/main/java/net/dzikoysk/funnyguilds/guild/permission/GuildPermissionController.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/guild/permission/GuildPermissionController.java
@@ -1,5 +1,6 @@
 package net.dzikoysk.funnyguilds.guild.permission;
 
+import net.dzikoysk.funnyguilds.guild.Guild;
 import net.dzikoysk.funnyguilds.user.User;
 import panda.std.Option;
 
@@ -7,33 +8,32 @@ public interface GuildPermissionController {
 
     /**
      * Get the value of a specific permission for a user
+     * @param guild the guild to check
      * @param user the user to check
      * @param permission the permission to check
      * @return the value of the permission, or an empty option if not set
      * @param <T> the type of the permission value
      */
-    <T> Option<T> getPermissionValue(User user, GuildPermission<T> permission);
-
-    /**
-     * Check if a user can perform an action based on a permission and required value
-     * @param user the user to check
-     * @param permission the permission to check
-     * @param requiredValue the required value to perform the action
-     * @return true if the user can perform the action, false otherwise
-     * @param <T> the type of the permission value
-     */
-    default <T> boolean canPerformAction(User user, GuildPermission<T> permission, T requiredValue) {
-        return this.getPermissionValue(user, permission)
-                .map(value -> value.equals(requiredValue))
-                .isPresent();
+    <T> Option<T> getPermissionValue(Guild guild, User user, GuildPermission<T> permission);
+    
+    default <T> Option<T> getPermissionValue(User user, GuildPermission<T> permission) {
+        return user.getGuild()
+                .flatMap(guild -> this.getPermissionValue(guild, user, permission));
     }
     
     /**
      * Check if a user can perform an action based on a boolean permission
+     * @param guild the guild to check
      * @param user the user to check
      * @param permission the permission to check
      * @return true if the user can perform the action, false otherwise
      */
+    default boolean canPerformAction(Guild guild, User user, GuildPermission<Boolean> permission) {
+        return this.getPermissionValue(guild, user, permission)
+                .filter(value -> value)
+                .isPresent();
+    }
+    
     default boolean canPerformAction(User user, GuildPermission<Boolean> permission) {
         return this.getPermissionValue(user, permission)
                 .filter(value -> value)
