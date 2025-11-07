5c384a225e0d136b36567f0b39d0a83fda8d77cc
Piotr Karnak <github@peri.dev>
2025-11-07 11:21:34 +0100

    Extend GuildPermissionController to handle protection-related events

--- plugin/src/main/java/net/dzikoysk/funnyguilds/guild/permission/FunnyGuildPermissionController.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/guild/permission/FunnyGuildPermissionController.java
@@ -1,21 +1,75 @@
 package net.dzikoysk.funnyguilds.guild.permission;
 
-import net.dzikoysk.funnyguilds.event.FunnyEvent;
 import net.dzikoysk.funnyguilds.event.SimpleEventHandler;
 import net.dzikoysk.funnyguilds.guild.Guild;
+import net.dzikoysk.funnyguilds.guild.permission.event.GuildBlockPermissionProtectionEvent;
+import net.dzikoysk.funnyguilds.guild.permission.event.GuildBucketPermissionProtectionEvent;
+import net.dzikoysk.funnyguilds.guild.permission.event.GuildEntityPermissionProtectionEvent;
+import net.dzikoysk.funnyguilds.guild.permission.event.GuildHangingPermissionProtectionEvent;
 import net.dzikoysk.funnyguilds.guild.permission.event.GuildPermissionCheckEvent;
+import net.dzikoysk.funnyguilds.guild.permission.event.GuildPermissionEvent;
+import net.dzikoysk.funnyguilds.guild.permission.event.GuildPermissionProtectionEvent;
 import net.dzikoysk.funnyguilds.user.User;
+import org.bukkit.event.Event;
+import org.bukkit.event.block.BlockEvent;
+import org.bukkit.event.entity.EntityEvent;
+import org.bukkit.event.hanging.HangingEvent;
+import org.bukkit.event.player.PlayerBucketEvent;
 import panda.std.Option;
 
 public class FunnyGuildPermissionController implements GuildPermissionController {
 
     @Override
     public <T> Option<T> getPermissionValue(Guild guild, User user, GuildPermission<T> permission) {
-        GuildPermissionCheckEvent event = GuildPermissionCheckEvent.of(FunnyEvent.EventCause.SYSTEM, user, guild, permission);
+        GuildPermissionCheckEvent event = new GuildPermissionCheckEvent(guild, user, permission);
         if (!SimpleEventHandler.handle(event)) {
             return Option.none();
         }
         return event.getPermissionValue();
     }
-    
+
+    @Override
+    public <T> Option<T> getProtectionPermissionValue(Guild guild, User user, GuildPermission<T> permission, Event event) {
+        GuildPermissionProtectionEvent protectionEvent;
+        if (event instanceof BlockEvent) {
+            protectionEvent = new GuildBlockPermissionProtectionEvent(
+                    GuildPermissionEvent.EventCause.SYSTEM,
+                    guild,
+                    user,
+                    permission,
+                    (BlockEvent) event
+            );
+        } else if (event instanceof PlayerBucketEvent) {
+            protectionEvent = new GuildBucketPermissionProtectionEvent(
+                    GuildPermissionEvent.EventCause.SYSTEM,
+                    guild,
+                    user,
+                    permission,
+                    (PlayerBucketEvent) event
+            );
+        } else if (event instanceof HangingEvent) {
+            protectionEvent = new GuildHangingPermissionProtectionEvent(
+                    GuildPermissionEvent.EventCause.SYSTEM,
+                    guild,
+                    user,
+                    permission,
+                    (HangingEvent) event
+            );
+        } else if (event instanceof EntityEvent) {
+            protectionEvent = new GuildEntityPermissionProtectionEvent(
+                    GuildPermissionEvent.EventCause.SYSTEM,
+                    guild,
+                    user,
+                    permission,
+                    (EntityEvent) event
+            );
+        } else {
+            throw new IllegalArgumentException("Unsupported event type: " + event.getClass().getName());
+        }
+        
+        if (!SimpleEventHandler.handle(protectionEvent)) {
+            return Option.none();
+        }
+        return protectionEvent.getPermissionValue();
+    }
 }
--- plugin/src/main/java/net/dzikoysk/funnyguilds/guild/permission/GuildPermissionController.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/guild/permission/GuildPermissionController.java
@@ -2,6 +2,7 @@ package net.dzikoysk.funnyguilds.guild.permission;
 
 import net.dzikoysk.funnyguilds.guild.Guild;
 import net.dzikoysk.funnyguilds.user.User;
+import org.bukkit.event.Event;
 import panda.std.Option;
 
 public interface GuildPermissionController {
@@ -22,6 +23,22 @@ public interface GuildPermissionController {
     }
     
     /**
+     * Get the value of a specific protection-related permission for a user
+     * @param guild the guild to check
+     * @param user the user to check
+     * @param permission the permission to check
+     * @param event the event related to the protection action
+     * @return the value of the permission, or an empty option if not set
+     * @param <T> the type of the permission value
+     */
+    <T> Option<T> getProtectionPermissionValue(Guild guild, User user, GuildPermission<T> permission, Event event);
+    
+    default <T> Option<T> getProtectionPermissionValue(User user, GuildPermission<T> permission, Event event) {
+        return user.getGuild()
+                .flatMap(guild -> this.getProtectionPermissionValue(guild, user, permission, event));
+    }
+    
+    /**
      * Check if a user can perform an action based on a boolean permission
      * @param guild the guild to check
      * @param user the user to check
@@ -36,6 +53,26 @@ public interface GuildPermissionController {
     
     default boolean canPerformAction(User user, GuildPermission<Boolean> permission) {
         return this.getPermissionValue(user, permission)
+                .filter(value -> value)
+                .isPresent();
+    }
+    
+    /**
+     * Check if a user can perform a protection-related action based on a boolean permission
+     * @param guild the guild to check
+     * @param user the user to check
+     * @param permission the permission to check
+     * @param event the event related to the protection action
+     * @return true if the user can perform the action, false otherwise
+     */
+    default boolean canPerformProtectionAction(Guild guild, User user, GuildPermission<Boolean> permission, Event event) {
+        return this.getProtectionPermissionValue(guild, user, permission, event)
+                .filter(value -> value)
+                .isPresent();
+    }
+    
+    default boolean canPerformProtectionAction(User user, GuildPermission<Boolean> permission, Event event) {
+        return this.getProtectionPermissionValue(user, permission, event)
                 .filter(value -> value)
                 .isPresent();
     }
