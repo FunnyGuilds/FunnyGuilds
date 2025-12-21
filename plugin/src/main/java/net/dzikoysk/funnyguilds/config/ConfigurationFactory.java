9594d07c16bd8b76fdf23a37e605cb182145203d
Piotr Karnak <github@peri.dev>
2025-12-21 23:43:48 +0100

    Drop migrations

--- plugin/src/main/java/net/dzikoysk/funnyguilds/config/ConfigurationFactory.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/config/ConfigurationFactory.java
@@ -8,22 +8,6 @@ import eu.okaeri.configs.yaml.bukkit.YamlBukkitConfigurer;
 import java.io.File;
 import net.dzikoysk.funnyguilds.FunnyGuilds;
 import net.dzikoysk.funnyguilds.config.message.MessageConfiguration;
-import net.dzikoysk.funnyguilds.config.migration.M0001_Migrate_old_region_notification_keys;
-import net.dzikoysk.funnyguilds.config.migration.M0002_Migrate_old_rank_kill_message;
-import net.dzikoysk.funnyguilds.config.migration.P0001_Fix_freecam_compensation_key_case;
-import net.dzikoysk.funnyguilds.config.migration.P0002_Migrate_old_heart_configuration;
-import net.dzikoysk.funnyguilds.config.migration.P0003_Migrate_old_tnt_protection_configuration;
-import net.dzikoysk.funnyguilds.config.migration.P0004_Migrate_tablist_into_separate_file;
-import net.dzikoysk.funnyguilds.config.migration.P0005_Fix_heart_configuration_centery_key;
-import net.dzikoysk.funnyguilds.config.migration.P0006_Migrate_old_scoreboard_configuration;
-import net.dzikoysk.funnyguilds.config.migration.P0007_Migrate_old_relational_tag_configuration;
-import net.dzikoysk.funnyguilds.config.migration.P0008_Migrate_old_heart_center_configuration;
-import net.dzikoysk.funnyguilds.config.migration.P0009_Migrate_old_killer_notification_key;
-import net.dzikoysk.funnyguilds.config.migration.P0010_Migrate_old_enlarge_enable_option;
-import net.dzikoysk.funnyguilds.config.migration.P0011_Migrate_old_security_system_keys;
-import net.dzikoysk.funnyguilds.config.migration.P0012_Migrate_old_broadcast_death_message_key;
-import net.dzikoysk.funnyguilds.config.migration.T0001_Update_player_list_animated;
-import net.dzikoysk.funnyguilds.config.migration.T0002_Update_tablist_keys;
 import net.dzikoysk.funnyguilds.config.serdes.ColorSerializer;
 import net.dzikoysk.funnyguilds.config.serdes.DecolorTransformer;
 import net.dzikoysk.funnyguilds.config.serdes.EntityTypeTransformer;
@@ -59,11 +43,6 @@ public final class ConfigurationFactory {
             });
             it.saveDefaults();
             it.load(true);
-
-            it.migrate(
-                    new M0001_Migrate_old_region_notification_keys(),
-                    new M0002_Migrate_old_rank_kill_message()
-            );
         });
     }
 
@@ -89,21 +68,6 @@ public final class ConfigurationFactory {
             });
             it.saveDefaults();
             it.load(true);
-
-            it.migrate(
-                    new P0001_Fix_freecam_compensation_key_case(),
-                    new P0002_Migrate_old_heart_configuration(),
-                    new P0003_Migrate_old_tnt_protection_configuration(),
-                    new P0004_Migrate_tablist_into_separate_file(),
-                    new P0005_Fix_heart_configuration_centery_key(),
-                    new P0006_Migrate_old_scoreboard_configuration(),
-                    new P0007_Migrate_old_relational_tag_configuration(),
-                    new P0008_Migrate_old_heart_center_configuration(),
-                    new P0009_Migrate_old_killer_notification_key(),
-                    new P0010_Migrate_old_enlarge_enable_option(),
-                    new P0011_Migrate_old_security_system_keys(),
-                    new P0012_Migrate_old_broadcast_death_message_key()
-            );
         });
     }
 
@@ -123,11 +87,6 @@ public final class ConfigurationFactory {
             });
             it.saveDefaults();
             it.load(true);
-
-            it.migrate(
-                    new T0001_Update_player_list_animated(),
-                    new T0002_Update_tablist_keys()
-            );
         });
     }
 
