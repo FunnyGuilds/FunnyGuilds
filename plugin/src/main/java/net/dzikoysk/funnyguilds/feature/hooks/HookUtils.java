f9fa28e188dc39f6c10ed125a90544aa7b3cc840
Piotr Karnak <github@peri.dev>
2025-12-26 21:54:47 +0100

    Bring back placeholderapi support

--- plugin/src/main/java/net/dzikoysk/funnyguilds/feature/hooks/HookUtils.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/feature/hooks/HookUtils.java
@@ -1,23 +1,63 @@
 package net.dzikoysk.funnyguilds.feature.hooks;
 
+import java.util.regex.Pattern;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextReplacementConfig;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.entity.Player;
 
 public final class HookUtils {
 
+    private static final Pattern BRACKET_PLACEHOLDER_PATTERN = Pattern.compile("[{]([^{}]+)[}]");
+    private static final LegacyComponentSerializer LEGACY_SECTION = LegacyComponentSerializer.legacySection();
+
     private HookUtils() {
     }
 
-    public static Component replacePlaceholders(Player observer, Player target, Component message) {
-        //return HookManager.PLACEHOLDER_API.map(api -> api.replacePlaceholders(observer, target, message)).orElseGet(message);
-        //throw new UnsupportedOperationException("Not implemented yet"); //TODO: reimplment
-        return message;
+    public static Component replacePlaceholders(
+            Player observer,
+            Player target,
+            Component message
+    ) {
+        return HookManager.PLACEHOLDER_API
+                .map(api -> {
+                    TextReplacementConfig replacement = TextReplacementConfig.builder()
+                            .match(BRACKET_PLACEHOLDER_PATTERN)
+                            .replacement((result, input) -> {
+                                String placeholder = result.group(1);
+                                String replaced = api.replacePlaceholders(
+                                        observer,
+                                        target,
+                                        "%" + placeholder + "%"
+                                );
+                                return LEGACY_SECTION.deserialize(replaced);
+                            })
+                            .build();
+                    return message.replaceText(replacement);
+                })
+                .orElseGet(message);
     }
 
-    public static Component replacePlaceholders(Player player, Component message) {
-        //return HookManager.PLACEHOLDER_API.map(api -> api.replacePlaceholders(player, message)).orElseGet(message);
-        //throw new UnsupportedOperationException("Not implemented yet"); //TODO: reimplment
-        return message;
+    public static Component replacePlaceholders(
+            Player player,
+            Component message
+    ) {
+        return HookManager.PLACEHOLDER_API
+                .map(api -> {
+                    TextReplacementConfig replacement = TextReplacementConfig.builder()
+                            .match(BRACKET_PLACEHOLDER_PATTERN)
+                            .replacement((result, input) -> {
+                                String placeholder = result.group(1);
+                                String replaced = api.replacePlaceholders(
+                                        player,
+                                        "%" + placeholder + "%"
+                                );
+                                return LEGACY_SECTION.deserialize(replaced);
+                            })
+                            .build();
+                    return message.replaceText(replacement);
+                })
+                .orElseGet(message);
     }
 
 }
