ada477609aa5396b05739e6bbacf69fe8263f76e
Piotr Karnak <github@peri.dev>
2025-12-27 11:39:15 +0100

    Remove legacy StatisticsAccessor#getPing

--- nms/v1_21_9/src/main/java/net/dzikoysk/funnyguilds/nms/v1_21_9/statistics/V1_21_9StatisticsAccessor.java
+++ nms/v1_21_9/src/main/java/net/dzikoysk/funnyguilds/nms/v1_21_9/statistics/V1_21_9StatisticsAccessor.java
@@ -3,7 +3,6 @@ package net.dzikoysk.funnyguilds.nms.v1_21_9.statistics;
 import net.dzikoysk.funnyguilds.nms.api.statistics.StatisticsAccessor;
 import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.CraftServer;
-import org.bukkit.entity.Player;
 
 public class V1_21_9StatisticsAccessor implements StatisticsAccessor {
 
@@ -15,11 +14,6 @@ public class V1_21_9StatisticsAccessor implements StatisticsAccessor {
     @Override
     public int getReloadCount() {
         return ((CraftServer) Bukkit.getServer()).reloadCount;
-    }
-
-    @Override
-    public int getPlayerPing(Player player) {
-        return player.getPing();
     }
 
 }
