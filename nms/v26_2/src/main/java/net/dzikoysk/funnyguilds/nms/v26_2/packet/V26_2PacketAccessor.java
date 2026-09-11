b61b5cc8c9a31f3f7b8a11111afa88ca781f9230
zrdzn <zrdzn9@gmail.com>
2026-09-11 19:32:09 +0200

    Add support for new interact and attack packets

---
+++ nms/v26_2/src/main/java/net/dzikoysk/funnyguilds/nms/v26_2/packet/V26_2PacketAccessor.java
@@ -0,0 +1,40 @@
+package net.dzikoysk.funnyguilds.nms.v26_2.packet;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelPipeline;
+import net.dzikoysk.funnyguilds.nms.api.network.GenericInboundChannelHandlerInstaller;
+import net.dzikoysk.funnyguilds.nms.api.network.GenericOutboundChannelHandlerInstaller;
+import net.dzikoysk.funnyguilds.nms.api.packet.FunnyGuildsInboundChannelHandler;
+import net.dzikoysk.funnyguilds.nms.api.packet.FunnyGuildsOutboundChannelHandler;
+import net.dzikoysk.funnyguilds.nms.api.packet.PacketAccessor;
+import net.minecraft.server.level.ServerPlayer;
+import org.bukkit.craftbukkit.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class V26_2PacketAccessor implements PacketAccessor {
+
+    private final GenericInboundChannelHandlerInstaller<?> inboundChannelHandlerInstaller =
+            new GenericInboundChannelHandlerInstaller<>(V26_2FunnyGuildsInboundChannelHandler::new);
+    private final GenericOutboundChannelHandlerInstaller<?> outboundChannelHandlerInstaller =
+            new GenericOutboundChannelHandlerInstaller<>(V26_2FunnyGuildsOutboundChannelHandler::new);
+
+    @Override
+    public FunnyGuildsInboundChannelHandler getOrInstallInboundChannelHandler(Player player) {
+        ServerPlayer serverPlayer = ((CraftPlayer) player).getHandle();
+
+        Channel channel = serverPlayer.connection.connection.channel;
+        ChannelPipeline pipeline = channel.pipeline();
+
+        return this.inboundChannelHandlerInstaller.installChannelHandlerInPipeline(pipeline);
+    }
+
+    @Override
+    public FunnyGuildsOutboundChannelHandler getOrInstallOutboundChannelHandler(Player player) {
+        ServerPlayer serverPlayer = ((CraftPlayer) player).getHandle();
+
+        Channel channel = serverPlayer.connection.connection.channel;
+        ChannelPipeline pipeline = channel.pipeline();
+
+        return this.outboundChannelHandlerInstaller.installChannelHandlerOutPipeline(pipeline);
+    }
+}
